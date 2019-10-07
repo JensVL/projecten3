e6c56dfa2c59a4c10428b9916d9679502f6130d0
LAURENS BLANCQUAERTCASSAER <laurens.blancquaertcassaer@student.hogent.be>
2019-10-07 03:38:45 +0200

    InitialisatieScript + AD/DNS installatie + SCCM preparation
    3 scripts geschreven die elkaar opvolgen:
    1) RUNFIRST initialisatiescript die de servernaam verandert en reboot
    2)  INSTALLDCDNS script dat basic instellingen van server wijzigt zoals ip adres  tijdzone, alsook installatie van AD en DNS
    3) PepareADforSCCM script dat de nodige voorbereidingen doet om SCCM installatie (op papa2 server) mogelijk te maken

---
+++ Servers/Alfa2/Testomgeving/Scripts/3_ConfigDCDNS.ps1
@@ -0,0 +1,8 @@
+
+# VARIABLES:
+
+
+# LOG SCRIPT TO FILE (+ op het einde van het script Stop-Transcript doen):
+Start-Transcript Start-Transcript "C:\ScriptLogs\3_ConfigDCDNSlog.txt"
+
+Stop-Transcript
