f712ce40e17cc22e92e8f3e589965bd20295c9e3
Tim Grijp <tim.grijp@gmail.com>
2019-10-07 17:05:13 +0200

    credentials geautomatiseerd

--- Servers/Mike2/scripts/prerequisites.ps1
+++ Servers/Mike2/scripts/prerequisites.ps1
@@ -6,12 +6,15 @@ New-NetIPAddress -IPAddress 172.18.1.3 -PrefixLength 24 -InterfaceIndex (Get-Net
 // configure dns 
 Set-DnsClientServerAddress -interfaceAlias ethernet -serveraddresses 172.18.1.66
 
-//change domain 
-//moet nog verbeterd worden zodat credentials automatisch zijn moet adds credentials zijn
- Add-Computer -DomainName red.local
+//change domain met ADDS Credentials
+
+$password = ConvertTo-SecureString "Admin19" -AsPlainText -Force
+$Cred = New-Object System.Management.Automation.PSCredential ("Administrator", $password)
+Add-Computer -DomainName red -Credential $Cred
+
 
 //change computername met adds credentials
-rename-computer -computername "$env:computername" -newname "mike2" -domaincredential Administrator'
+rename-computer -computername "$env:computername" -newname "mike2" -DomainCredential $cred
 
 //install prerequisites voor sharepoint moet access tot internet hebben  
 //dit hangt af van waar de shared folder is
