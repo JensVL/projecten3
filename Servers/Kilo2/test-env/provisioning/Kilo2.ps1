e0ad6377498db0b5ef17e69903c7607960ce5985
Tibo Vanhercke <tibo.vanhercke@student.hogent.be>
2019-10-07 17:03:35 +0200

    Edited domain, Added DNS and Router, Added Lease

--- Servers/Kilo2/test-env/provisioning/Kilo2.ps1
+++ Servers/Kilo2/test-env/provisioning/Kilo2.ps1
@@ -1,9 +1,6 @@
-# Configureren van IP address van de DHCP Server
-# TODO
-
 # Toevoegen aan domain
 
-$domain = ??????????????????????????????????
+$domain = "red.local"
 $password = "Admin2019" | ConvertTo-SecureString -AsPlainText -Force
 $username = "Administrator"
 $credential = New-Object System.Management.Automation.PSCredential($username, $password)
@@ -19,12 +16,12 @@ Install-WindowsFeature -Name DHCP -IncludeManagementTools
 Add-DhcpServerV4Scope -Name "Vlan 200" -StartRange 172.18.0.2 -EndRange 172.18.0.254 -SubnetMask 255.255.255.0
 
 # DNS, Router, Default Gateway en mogelijk andere zaken toevoegen
-# TODO
+
+Set-DhcpServerV4OptionValue -DnsServer 172.18.1.66,172.18.1.67 -Router 172.18.0.1 -ScopeId 172.18.0.0
 
 # Lease time configureren
-# TODO
 
-
+Set-DhcpServerv4Scope -ScopeId 172.18.0.0 -LeaseDuration (New-TimeSpan -Days 2)
 
 # Restart DHCP Server
 Restart-service dhcpserver
