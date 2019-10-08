be4b04db1ac1494dff42e0069bcf3a6a0648d903
LAURENS BLANCQUAERTCASSAER <laurens.blancquaertcassaer@student.hogent.be>
2019-10-08 10:43:49 +0200

    DNS configuratie Deel 2
    Reverse lookup zone gemaakt + PTR record (mag wss weg kan zijn dat reverse lookup zone overbodig is aangezien we maar 1 domain hebben zonder subdomain

--- Servers/Alfa2/Testomgeving/Scripts/3_ConfigDCDNS.ps1
+++ Servers/Alfa2/Testomgeving/Scripts/3_ConfigDCDNS.ps1
@@ -25,9 +25,9 @@ $InformationPreference = "Continue"
 Start-Transcript "C:\ScriptLogs\3_ConfigDCDNSlog.txt"
 
 # 1) Stel forward primary lookup zones in voor alle servers in het red domein:
-Add-DnsServerPrimaryZone -Name "red.local" -ReplicationScope "Domain" -ZoneFile "red.local.dns" -DynamicUpdate "Secure"
+Add-DnsServerPrimaryZone -Name "red.local" -ReplicationScope "Domain" -DynamicUpdate "Secure"
 
-# 2) Voeg de servers als AAAA records toe met hun ip adres in de aangemaakte primary zone:
+# 2) Voeg de servers als AAAA records toe met hun ip adres in de aangemaakte primary zone: (name to ip address)
 # LET OP: Charlie2 Exchange (mail) server = MX record ipv AAAA record en Bravo2 DC2 = NS record
 # TODO Bravo2 en Charlie2 toevoegen als MX en NS record
 Add-DnsServerResourceRecordA -Name "Delta2" -ZoneName "red.local" -IPv4Address "$Delta2IP" -CreatePtr
@@ -38,7 +38,14 @@ Add-DnsServerResourceRecordA -Name "November2" -ZoneName "red.local" -IPv4Addres
 Add-DnsServerResourceRecordA -Name "Oscar2" -ZoneName "red.local" -IPv4Address "$Oscar2IP" -CreatePtr
 Add-DnsServerResourceRecordA -Name "Papa2" -ZoneName "red.local" -IPv4Address "$Papa2IP" -CreatePtr
 
-# TODO: Eventueel reverse lookupzone maken
+# 3) DNS reverse lookup zone maken (IP address to domain name):
+# 172.18.1.66 / 27 => Network portion: 172.18.1.64
+# TODO: MAG WSS WEG reverse lookup zone enkel voor als je subdomains hebt in andere subnets (wij hebben maar 1 domain = red.local)
+Add-DnsServerPrimaryZone -ReplicationScope "Domain" -DynamicUpdate "Secure" -NetworkID "172.18.1.64/27"
+
+# Voeg de servers van het red domain toe als PTR records in de reverse lookup zone:
+# TODO: MAG WSS WEG reverse lookup zone enkel voor als je subdomains hebt in andere subnets (wij hebben maar 1 domain = red.local)
+Add-DnsServerResourceRecordPtr -Name "Delta2" -ZoneName "1.18.172.in-addr.arpa" -AllowUpdateAny --PtrDomainName "Delta2.contoso.com"
 
 # 3) DNS forwarders instellen op de Hogent DNS servers:          DNS FORWARDERS MOETEN OF NIET??????????????????????????????
 Add-DnsServerForwarder -IpAddress "193.190.173.1","193.190.173.2"
