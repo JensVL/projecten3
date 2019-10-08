2bee2f5f20d7347b70988d2f86a8d2f2b7361e40
LAURENS BLANCQUAERTCASSAER <laurens.blancquaertcassaer@student.hogent.be>
2019-10-08 11:35:41 +0200

    DNS Deel 3 (laatste deel)
    - Primary zone ingesteld zodat hij automatisch repliceert naar secondary zone (DC2 / DNS2)
    -MX record toegevoegd voor Charlie2 mail server

--- Servers/Alfa2/Testomgeving/Scripts/3_ConfigDCDNS.ps1
+++ Servers/Alfa2/Testomgeving/Scripts/3_ConfigDCDNS.ps1
@@ -5,8 +5,8 @@
 $VBOXdrive = "Z:"
 
 #----Servers in Red domein hun ipadressen:
-  $Bravo2IP = "172.18.1.67" # DC2 / DNS2      -----------------------(TODO: DNS MOET NS RECORD HEBBEN GEEN AAAA RECORD)
-  $Charlie2IP = "172.18.1.68" # Exchange Server --------------------- TODO: MAIL SERVER IS MX RECORD GEEN AAAA RECORD!)
+  $Bravo2IP = "172.18.1.67" # DC2 / DNS2
+  $Charlie2IP = "172.18.1.68" # Exchange Server
   $Delta2IP = "172.18.1.69" # IIS Webserver
   $Kilo2IP = "172.18.1.1" # DHCP Server
   $Lima2IP = "172.18.1.2" # File Server
@@ -25,11 +25,16 @@ $InformationPreference = "Continue"
 Start-Transcript "C:\ScriptLogs\3_ConfigDCDNSlog.txt"
 
 # 1) Stel forward primary lookup zones in voor alle servers in het red domein:
-Add-DnsServerPrimaryZone -Name "red.local" -ReplicationScope "Domain" -DynamicUpdate "Secure"
+# TODO: Checken of DC2 / DNS2 automatisch een NS record heeft (voor replicatie TransferToZoneNameServer)
+Add-DnsServerPrimaryZone -Name "red.local" -ReplicationScope "Domain" -DynamicUpdate "Secure" -SecureSecondaries "TransferToZoneNameServer"
 
 # 2) Voeg de servers als AAAA records toe met hun ip adres in de aangemaakte primary zone: (name to ip address)
-# LET OP: Charlie2 Exchange (mail) server = MX record ipv AAAA record en Bravo2 DC2 = NS record
-# TODO Bravo2 en Charlie2 toevoegen als MX en NS record
+# LET OP: Charlie2 Exchange (mail) server = MX record ipv AAAA record
+#en Bravo2 DC2 = NS record (NS record niet zelf aanmaken gebeurd automatisch volgens AD-integration zones)
+# MX record -MailExchange option moet pointen naar bestaande A record (Zie -Mail Exchange optie Microsoft docs Add-DnsServerResourceRecordMX)
+Add-DnsServerResourceRecordA -Name "Charlie2" -ZoneName "red.local" -IPv4Address "$Charlie2IP" -CreatePtr
+Add-DnsServerResourceRecordMX -Name "." -MailExchange "Charlie2.red.local" -ZoneName "red.local"
+
 Add-DnsServerResourceRecordA -Name "Delta2" -ZoneName "red.local" -IPv4Address "$Delta2IP" -CreatePtr
 Add-DnsServerResourceRecordA -Name "Kilo2" -ZoneName "red.local" -IPv4Address "$Kilo2IP" -CreatePtr
 Add-DnsServerResourceRecordA -Name "Lima2" -ZoneName "red.local" -IPv4Address "$Lima2IP" -CreatePtr
@@ -38,6 +43,7 @@ Add-DnsServerResourceRecordA -Name "November2" -ZoneName "red.local" -IPv4Addres
 Add-DnsServerResourceRecordA -Name "Oscar2" -ZoneName "red.local" -IPv4Address "$Oscar2IP" -CreatePtr
 Add-DnsServerResourceRecordA -Name "Papa2" -ZoneName "red.local" -IPv4Address "$Papa2IP" -CreatePtr
 
+#######################################################################################################################################################
 # 3) DNS reverse lookup zone maken (IP address to domain name):
 # 172.18.1.66 / 27 => Network portion: 172.18.1.64
 # TODO: MAG WSS WEG reverse lookup zone enkel voor als je subdomains hebt in andere subnets (wij hebben maar 1 domain = red.local)
@@ -46,6 +52,7 @@ Add-DnsServerPrimaryZone -ReplicationScope "Domain" -DynamicUpdate "Secure" -Net
 # Voeg de servers van het red domain toe als PTR records in de reverse lookup zone:
 # TODO: MAG WSS WEG reverse lookup zone enkel voor als je subdomains hebt in andere subnets (wij hebben maar 1 domain = red.local)
 Add-DnsServerResourceRecordPtr -Name "Delta2" -ZoneName "1.18.172.in-addr.arpa" -AllowUpdateAny --PtrDomainName "Delta2.contoso.com"
+#######################################################################################################################################################
 
 # 3) DNS forwarders instellen op de Hogent DNS servers:          DNS FORWARDERS MOETEN OF NIET??????????????????????????????
 Add-DnsServerForwarder -IpAddress "193.190.173.1","193.190.173.2"
