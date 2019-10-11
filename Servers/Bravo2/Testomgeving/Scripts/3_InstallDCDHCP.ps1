488741e0cf0c4ad56d64341d75f1d221a8b2738a
Arno Van Nieuwenhove <arno.vannieuwenhove@student.hogent.be>
2019-10-11 19:00:14 +0200

    DHCP

--- Servers/Bravo2/Testomgeving/Scripts/3_InstallDCDHCP.ps1
+++ Servers/Bravo2/Testomgeving/Scripts/3_InstallDCDHCP.ps1
@@ -1,5 +1,5 @@
 ## Static IP address
-New-NetIPAddress -InterfaceIndex 2 -IPAddress 192.168.64.2 -PrefixLength 24 -DefaultGateway 192.168.64.1
+New-NetIPAddress -InterfaceIndex 2 -IPAddress 172.18.1.67 -PrefixLength 24 -DefaultGateway 192.168.64.1
 Install-WindowsFeature DHCP -IncludeManagementTools
 ## DHCP scope
 Add-DHCPServerv4Scope -Name “Employee Scope” -StartRange 192.168.64.10 -EndRange 192.168.64.30 -SubnetMask 255.255.255.0 -State Active
