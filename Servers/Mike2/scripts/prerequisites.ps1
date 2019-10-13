652d0ce662afe881565acf7cc41304f7deed057b
Tim Grijp <tim.grijp.w2040@student.hogent.be>
2019-10-14 00:30:26 +0200

    scripts opgesplitst en uitgebreid

--- Servers/Mike2/scripts/prerequisites.ps1
+++ Servers/Mike2/scripts/prerequisites.ps1
@@ -1,34 +1,21 @@
-
-
-// configure hostonlyadapter
-New-NetIPAddress -IPAddress 172.18.1.3 -PrefixLength 24 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
-
-// configure dns 
-Set-DnsClientServerAddress -interfaceAlias ethernet -serveraddresses 172.18.1.66
-
-//change domain met ADDS Credentials
-
-$password = ConvertTo-SecureString "Admin19" -AsPlainText -Force
-$Cred = New-Object System.Management.Automation.PSCredential ("Administrator", $password)
-Add-Computer -DomainName red -Credential $Cred
-
-
-//change computername met adds credentials
-rename-computer -computername "$env:computername" -newname "mike2" -DomainCredential $cred
-
-//install prerequisites voor sharepoint moet access tot internet hebben  
-//dit hangt af van waar de shared folder is
+start-transcript "C:\ScriptLogs\2.txt"
+Write-host "Waiting 5 seconds before continuing"
+start-sleep -s 5
+Write-Output "installing prerequisites"
+# install prerequisites voor sharepoint moet access tot internet hebben  
+# dit hangt af van waar de shared folder is
   \\VBOXSVR\windows_school_vm\sharepoint\PrerequisiteInstaller.exe /unattended
 
-//install sql internet nodig
+# //install sql internet nodig
+# \\VBOXSVR\windows_school_vm\SQLServer2016-SSEI-Eval.exe  /Iacceptsqlserverlicenseterms /q
 
- \\VBOXSVR\windows_school_vm\SQLServer2016-SSEI-Eval.exe  /Iacceptsqlserverlicenseterms /q
-
-
+Write-host "Waiting 5 seconds before continuing"
+start-sleep -s 5
 
 
 
 #############################################################################################################################
+$password = ConvertTo-SecureString "Admin19" -AsPlainText -Force
 
 $farmcredentials = (New-Object System.Management.Automation.PSCredential ($DOMAIN + "\" + "Administrator"),($password))
 
@@ -62,6 +49,8 @@ Write-Output "Create the Central Administration site"
 New-SPCentralAdministration `
 -Port 11111 `
 -WindowsAuthProvider "NTLM"
+
+stop-transcript
 
 
 
