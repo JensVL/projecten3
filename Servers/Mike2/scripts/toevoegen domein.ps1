1614699b017e43807c8334501bd5e793c1c9fe11
Tim Grijp <tim.grijp.w2040@student.hogent.be>
2019-10-14 16:27:51 +0200

    [mike2] change script

--- Servers/Mike2/scripts/toevoegen domein.ps1
+++ Servers/Mike2/scripts/toevoegen domein.ps1
@@ -1,17 +1,20 @@
 start-transcript "C:\ScriptLogs\1.txt"
+
+$VBOXdrive = "Z:\"
+
 # Auto run script prerquisites na reboot
 
 Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name ResumeScript `
-                -Value 'C:\Windows\system32\WindowsPowerShell\v1.0\Powershell.exe -executionpolicy bypass -file "\\VBOXSVR\scripts voor mike2\prerequisites.ps1"'
+                -Value 'C:\Windows\system32\WindowsPowerShell\v1.0\Powershell.exe -executionpolicy bypass -file "$VBOXdrive\scripts voor mike2\prerequisites.ps1"'
 
 Write-Output "add password credentials"
 
-$password = ConvertTo-SecureString "Admin19" -AsPlainText -Force
+$password = ConvertTo-SecureString "Admin2019" -AsPlainText -Force
 $Cred = New-Object System.Management.Automation.PSCredential ("Administrator", $password)
 
 $Username = $Cred.GetNetworkCredential().UserName
 $Password = $Cred.GetNetworkCredential().Password
-$Uservalue= "red.local\$Username"
+$Uservalue= "red\$Username"
 
 # instellen velden voor automatisch in te loggen na opstarten
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value $Uservalue
@@ -28,27 +31,18 @@ New-NetIPAddress -IPAddress 172.18.1.3 -PrefixLength 24 -InterfaceIndex (Get-Net
 Write-Output "configure dns"
 Set-DnsClientServerAddress -interfaceAlias "Ethernet" -serveraddresses 172.18.1.66
 
-Write-host "Waiting 10 seconds before continuing"
-start-sleep -s 10
+Write-host "Waiting 15 seconds before continuing"
+start-sleep -s 15
 
-#change domain met ADDS Credentials
-
-Write-Output "set domain to red"
-Add-Computer -DomainName red -Credential $Cred
 
 # change computername met adds credentials
 
-Write-Output "set computername to mike2v5"
-rename-computer -computername "$env:computername" -newname "mike2" -DomainCredential $cred
+Write-Output "set computername to mike2 and add to domain red"
 
-Write-host "Waiting 10 seconds before continuing"
-start-sleep -s 10
-
-
+add-Computer -DomainName red.local -ComputerName "$env:computername" -newname mike2v6 -DomainCredential $Cred
 
 
 
 stop-transcript
 
 restart-computer
-
