a90a3e7ef892bf865a1dc18f6677a3a8abab0338
Cedric Detemmerman <cedricq9@gmail.com>
2019-10-14 16:14:43 +0200

    share permissions + max capacity per user

--- Servers/Lima2/LIMA2/provisioning/Lima2.ps1
+++ Servers/Lima2/LIMA2/provisioning/Lima2.ps1
@@ -1,21 +1,12 @@
-#Installeert de File-Services rol
-if (-not (Get-WindowsFeature -Name File-Services).Installed)
-{
-    Install-WindowsFeature -Name File-Services
-}
-#Installeert de FS resource manager en management tools
-if (-not (Get-WindowsFeature -Name FS-Resource-Manager).Installed)
-{
-    Install-WindowsFeature –Name FS-Resource-Manager –IncludeManagementTools
-}
-#Init disk 0, zodat nieuwe partities aangemaakt kunnen worden. Daarna worden de volumes geformateerd.
+#install windows fileservices 
+
+Install-WindowsFeature File-Services
+Install-WindowsFeature –Name FS-Resource-Manager –IncludeManagementTools
 Get-Disk 0 | Initialize-Disk
-Resize-Partition -DiskNumber 0 -PartitionNumber 2 -Size (30GB)
 New-Partition -DiskNumber 0 -Size 5GB -DriveLetter D
 New-Partition -DiskNumber 0 -Size 5GB -DriveLetter E
 Format-Volume -DriveLetter D -FileSystem NTFS -NewFileSystemLabel VerkoopData -Confirm:$False
 Format-Volume -DriveLetter E -FileSystem NTFS -NewFileSystemLabel OntwikkelingData -Confirm:$False
-#Init disk 1
 Get-Disk 1 | Initialize-Disk
 New-Partition -DiskNumber 1 -Size 5GB -DriveLetter F
 New-Partition -DiskNumber 1 -Size 5GB -DriveLetter G
@@ -27,28 +18,46 @@ Format-Volume -DriveLetter G -FileSystem NTFS -NewFileSystemLabel DirData -Confi
 Format-Volume -DriveLetter H -FileSystem NTFS -NewFileSystemLabel AdminData -Confirm:$False
 Format-Volume -DriveLetter Y -FileSystem NTFS -NewFileSystemLabel HomeDirs -Confirm:$False
 Format-Volume -DriveLetter Z -FileSystem NTFS -NewFileSystemLabel ProfileDirs -Confirm:$False
-#Nieuwe shares worden aangemaak, benoemd en krijgen een pad.
-New-SmbShare -Name "VerkoopData" -Path "D:\"
-New-SmbShare -Name "OntwikkelingData" -Path "E:\"
-New-SmbShare -Name "ItData" -Path "F:\"
-New-SmbShare -Name "DirData" -Path "G:\"
-New-SmbShare -Name "AdminData" -Path "H:\"
-New-SmbShare -Name "HomeDirs" -Path "Y:\"
-New-SmbShare -Name "ProfileDirs" -Path "Z:\"
-#Configure shadow storage voor adminData
-vssadmin add shadowstorage /for=h: /on=h: /maxsize=2000mb
-#Hier komt ps code voor dagleijske schaduw copy te maken
+New-SmbShare -Name "VerkoopData" -Path "D:\" -ChangeAccess AvalonSoft\Administrators `  -FullAccess "AvalonSoft\sales"
+New-SmbShare -Name "OntwikkelingData" -Path "E:\" -ChangeAccess AvalonSoft\Administrators `  -FullAccess "AvalonSoft\technical"
+New-SmbShare -Name "ItData" -Path "F:\" -ChangeAccess AvalonSoft\Administrators `  -FullAccess "AvalonSoft\IT"
+New-SmbShare -Name "DirData" -Path "G:\" -ChangeAccess AvalonSoft\Administrators `  -FullAccess "AvalonSoft\management"
+New-SmbShare -Name "AdminData" -Path "H:\" -ChangeAccess AvalonSoft\Administrators `  -FullAccess "AvalonSoft\Administrators"
+New-SmbShare -Name "HomeDirs" -Path "Y:\" -ChangeAccess AvalonSoft\Administrators `  -FullAccess "AvalonSoft\*"
+New-SmbShare -Name "ProfileDirs" -Path "Z:\" -ChangeAccess AvalonSoft\Administrators `  -FullAccess "AvalonSoft\*"
+New-SmbShare -Name "ShareVerkoop" -Path "Z:\" -ChangeAccess AvalonSoft\Administrators `  -FullAccess "AvalonSoft\*"
 
+
+New-FsrmQuota -Path "D:\" -Description "limit usage to 0.2GB" -Size 200MB
+
+New-FsrmQuota -Path "E:\" -Description "limit usage to 200MB" -Size 200MB
+
+
+###Connecting to domain controller using PowerShell
+winrm quickconfig
+Set-ExecutionPolicy Unrestricted -F
+Netsh interface ip set wins name="Local Area Connection" source=static addr=10.0.0.10
+
+#(command not functional yet: keeps opening up credentials prompt AKA not automated! so use in powershell first)
+#Add-Computer -ComputerName "Lima2" -LocalCredential "Lima2\vagrant" -DomainName "AvalonSoft.net" -Credential vagrant\vagrant -Restart -Force -Verbose
+
+ 
 
 ###Creating a new file share:
 You'll need to make sure the folder path exists prior to running this command:
+New-Item "C:\Shared" –type directory
 New-SmbShare -Name Logs -Description "Log Files" -Path C:\Shares\Logs
 
 
-###Create groups (MUST BE DONE BY DC TECHNICALLY)
-
-
 ###Setting share permissions 
+#https://docs.microsoft.com/en-us/powershell/module/smbshare/new-smbshare?view=win10-ps
 #https://docs.microsoft.com/en-us/powershell/module/smbshare/grant-smbshareaccess?view=win10-ps
+#Show all AD users= Get-ADUser -Filter *
+#Grant-SmbShareAccess -Name Loggs -AccountName AvalonSoft\Managers -AccessRight Read
 
-Grant-SmbShareAccess -Name Loggs -AccountName AvalonSoft\Managers -AccessRight Read
+#New-SMBShare –Name "Shared" –Path "C:\Shared" ` -ChangeAccess AvalonSoft\Administrators `  -FullAccess "AvalonSoft\sales"
+
+###Setting maximum size for each share
+
+New-FsrmQuota -Path "C:\Shares" -Description "limit usage to 0.2GB" -Size 200MB
+
