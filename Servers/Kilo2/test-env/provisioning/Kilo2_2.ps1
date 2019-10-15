4062c33b011786d10123017485a0d4d77ae4d5ae
Tibo Vanhercke <tibo.vanhercke@student.hogent.be>
2019-10-15 14:13:28 +0200

    [Kilo2] Split Kilo2.ps1 file to support reboot while provisioning

---
+++ Servers/Kilo2/test-env/provisioning/Kilo2_2.ps1
@@ -0,0 +1,25 @@
+# Configureren van de Scopes op de DHCP Server
+
+# --scope vlan 200--
+Write-Host "Configuring VLAN 200..."
+Add-DhcpServerV4Scope -Name "Vlan 200" -StartRange 172.18.0.2 -EndRange 172.18.0.254 -SubnetMask 255.255.255.0 -Type Both
+
+# DNS, Router, Default Gateway en mogelijk andere zaken toevoegen
+
+Set-DhcpServerV4OptionValue -DnsServer 172.18.1.66 -Router 172.18.0.1 -ScopeId 172.18.0.0
+
+# Lease time configureren
+
+Set-DhcpServerv4Scope -ScopeId 172.18.0.0 -LeaseDuration (New-TimeSpan -Days 2)
+
+Write-Host "VLAN 200 configured"
+Write-Host "Authorizing DHCP server..."
+
+Start-Sleep -Seconds 10
+
+Add-DhcpServerInDC -DnsName "Kilo2.red.local"
+
+Write-Host "DHCP server authorized"
+
+# Restart DHCP Server
+Restart-service dhcpserver
