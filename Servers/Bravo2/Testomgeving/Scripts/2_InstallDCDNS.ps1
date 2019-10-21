304a63b8aaca7c9f6b344cb95f66c62621d5f7be
ArnoVanNieuwenhove <36506240+ArnoVanNieuwenhove@users.noreply.github.com>
2019-10-21 15:33:04 +0200

    Update 2_InstallDCDNS.ps1

--- Servers/Bravo2/Testomgeving/Scripts/2_InstallDCDNS.ps1
+++ Servers/Bravo2/Testomgeving/Scripts/2_InstallDCDNS.ps1
@@ -36,31 +36,31 @@ New-NetIPAddress -InterfaceAlias "$AdapterNaam" -IPAddress "$IpAddress" -PrefixL
 # Eventueel commenten tijdens testen in demo omgeving
 Set-DnsClientServerAddress -InterfaceAlias "$AdapterNaam" -ServerAddress "$IpAlfa2","$IpAddress"
 
-# 4) Installatie ADDS:
+# 5) Installatie ADDS:
 Write-host "Starting installation of ADDS role:" -ForeGroundColor "Green"
 Install-WindowsFeature AD-domain-services -IncludeManagementTools
 import-module ADDSDeployment
 
-# 5) Idem aan het 1_RUNFIRST.ps1 script zal deze registry instelling ervoor zorgen dat ons volgende script automatisch wordt geladen
+# 6) Idem aan het 1_RUNFIRST.ps1 script zal deze registry instelling ervoor zorgen dat ons volgende script automatisch wordt geladen
 # Want het installeren van de ADDS role herstart automatisch onze server
 # RunOnce verwijderd deze instelling automatisch nadat het script klaar is met runnen
 Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name ResumeScript `
                 -Value "C:\Windows\system32\WindowsPowerShell\v1.0\Powershell.exe -executionpolicy bypass -file `"$VBOXdrive\3_ConfigDCDNS.ps1`""
 
-# 6) DSRM instellen
+# 7) DSRM instellen
 $CurrentCredentials = Get-Credential -UserName "RED\$env:USERNAME" -Message "Geef je gewenste DSRM passwoord in"
 $DSRM = $CurrentCredentials.Password
 
-# 6.1) De eerste command zorgt ervoor dat je je als admin niet steeds moet inloggen wanneer je wijzigingen wil doen:
+# 7.1) De eerste command zorgt ervoor dat je je als admin niet steeds moet inloggen wanneer je wijzigingen wil doen:
 set-itemproperty -Path "REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
                 -name "ConsentPromptBehaviorAdmin" -value 0
 Set-ItemProperty -Path "REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
                 -Name "FilterAdministratorToken" -value 1
 
-# 7) Firewall uitschakelen
+# 8) Firewall uitschakelen
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
-# 8) Joinen van domein "Red.local":
+# 9) Joinen van domein "Red.local":
 Write-host "Starting configuration of red.local domain:" -ForeGroundColor "Green"
 install-ADDSDomainController -DomainName "red.local" `
                   -ReplicationSourceDC "Alfa2.red.local" `
