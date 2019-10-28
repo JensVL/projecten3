8e9da2da45d380b385cb90f93f37beb678bc62a0
Tim Grijp <tim.grijp.w2040@student.hogent.be>
2019-10-28 16:31:29 +0100

    delete folder

--- Servers/Mike2/scripts/SPsetup.ps1
+++
@@ -1,4 +0,0 @@
-Start-Process "Z:\sharepoint\officeserver\setup.exe" -ArgumentList "/config `"$PSScriptRoot\SPinstallation.xml`"" -WindowStyle Minimized -wait
-
-
-& "Z:\scripts voor mike2\SPfarm.ps1"
