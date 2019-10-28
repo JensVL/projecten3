f3819f12049fd266a8eaacb58bc8250b5cfe5a09
AronMarckx <aronmarckx@hotmail.com>
2019-10-28 17:04:50 +0100

    Update 4_SSMS.ps1

--- Servers/November2/Scripts/4_SSMS.ps1
+++ Servers/November2/Scripts/4_SSMS.ps1
@@ -1,6 +1,6 @@
 # Set file and folder path for SSMS installer .exe
 $folderpath="C:\Users\Administrator\Desktop"
-$filepath="$folderpath\SSMT-Setup-ENU.exe"
+$filepath="$folderpath\SSMS-Setup-ENU.exe"
  
 #If SSMS not present, download
 if (!(Test-Path $filepath)){
@@ -12,5 +12,5 @@ write-host "Located the SQL SSMS Installer binaries, moving on to install..."
  
 # start the SSMS installer
 write-host "Beginning SSMS install..." -nonewline
-Start-Proces -Filepath $filepath -ArgumentList -silent -Wait
+Start-Process -Filepath $filepath -ArgumentList -silent -Wait
 Write-Host "SSMS installation complete" -ForegroundColor Green
