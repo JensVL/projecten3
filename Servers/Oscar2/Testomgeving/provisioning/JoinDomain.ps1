27ca552237cd7688bafb2622f48d7e5821dc1620
Jonas Vandegehuchte <Jonas.Vandegehuchte.7330@student.hogent.be>
2019-10-28 15:11:02 +0100

    Aanpassen vagrant-host.yml

---
+++ Servers/Oscar2/Testomgeving/provisioning/JoinDomain.ps1
@@ -0,0 +1,5 @@
+Write-host "Servernaam wijzigen naar Oscar2" -ForeGroundColor "Green"
+Rename-Computer -NewName Oscar2 -Force
+
+Write-host "Oscar 2 toevoegen aan domein red.local" -ForeGroundColor "Green"
+Add-Computer -ComputerName 'Oscar2' -DomainName 'red.local'-Credential red.local\Administrator
