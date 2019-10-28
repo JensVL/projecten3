dfda2007daad7a03c470479122c12910514f0d57
Tibo Vanhercke <tibo.vanhercke@student.hogent.be>
2019-10-28 16:54:08 +0100

    Added use of jobs for all tasks

--- Servers/Kilo2/test-env/provisioning/Kilo2_1.ps1
+++ Servers/Kilo2/test-env/provisioning/Kilo2_1.ps1
@@ -5,31 +5,36 @@ Start-Job -Name InstallDHCP -ScriptBlock {
     Install-WindowsFeature -Name DHCP -IncludeManagementTools
 }
 
-Write-Host "Configuring IP..."
-$IP = "172.18.1.1"
-$DefaultGateway = "172.18.1.66"
-$DNS = "172.18.1.66"
-$InterfaceIndex = Get-NetIPAddress -IPAddress $IP | select -ExpandProperty InterfaceIndex
-Remove-NetIPAddress -InterfaceIndex $InterfaceIndex -Confirm:$false
-Remove-NetRoute -InterfaceIndex $InterfaceIndex -Confirm:$false
-Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ResetServerAddresses
-New-NetIPAddress -InterfaceIndex $InterfaceIndex -IPAddress $IP -PrefixLength 24 -DefaultGateway $DefaultGateway
-Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses $DNS
-Write-Host "Ip Configured"
+Start-Job -Name ConfiguringIP -ScriptBlock {
+    Write-Host "Configuring IP..."
+    $IP = "172.18.1.1"
+    $DefaultGateway = "172.18.1.66"
+    $DNS = "172.18.1.66"
+    $InterfaceIndex = Get-NetIPAddress -IPAddress $IP | select -ExpandProperty InterfaceIndex
+    Remove-NetIPAddress -InterfaceIndex $InterfaceIndex -Confirm:$false
+    Remove-NetRoute -InterfaceIndex $InterfaceIndex -Confirm:$false
+    Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ResetServerAddresses
+    New-NetIPAddress -InterfaceIndex $InterfaceIndex -IPAddress $IP -PrefixLength 24 -DefaultGateway $DefaultGateway
+    Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses $DNS
+    Start-Sleep -s 5
+    Write-Host "Ip Configured"
+}
 
 # Add delay to give the server time to adapt to new ip configurations
 # If no delay is added the Machine will not be adapted to the new ip configurations and will fail to join the domain
 
-Start-Sleep -s 5
-
+Wait-Job -Name ConfiguringIP
 # Toevoegen aan domain
-Write-Host "Adding server to Domain..."
-$domain = "red.local"
-$password = "vagrant" | ConvertTo-SecureString -AsPlainText -Force
-$username = "Administrator"
-$credential = New-Object System.Management.Automation.PSCredential($username, $password)
+Start-Job -Name AddToDomain -ScriptBlock {
+    Write-Host "Adding server to Domain..."
+    $domain = "red.local"
+    $password = "vagrant" | ConvertTo-SecureString -AsPlainText -Force
+    $username = "Administrator"
+    $credential = New-Object System.Management.Automation.PSCredential($username, $password)
 
-Add-Computer -DomainName $domain -Credential $credential -Force
-Write-Host "Server added to Domain"
+    Add-Computer -DomainName $domain -Credential $credential -Force
+    Write-Host "Server added to Domain"
+}
 
-Wait-Job -Name InstallDHCP
+Wait-Job
+Receive-Job -Name KeyboardSetting, ConfiguringIP, AddToDomain, InstallDHCP
--- Servers/Kilo2/test-env/provisioning/Kilo2_2.ps1
+++ Servers/Kilo2/test-env/provisioning/Kilo2_2.ps1
@@ -1,37 +1,38 @@
+$j = @()
 # Creating security groups
-
-Write-Host "Creating security groups..."
-cmd.exe /c "netsh dhcp add securitygroups"
-Write-Host "Security Groups created"
-Restart-Service dhcpserver
-
+$j += Start-Job -Name SecurityGroups -ScriptBlock {
+    Write-Host "Creating security groups..."
+    cmd.exe /c "netsh dhcp add securitygroups"
+    Write-Host "Security Groups created"
+}
 # Configureren van de Scopes op de DHCP Server
 
 # --scope vlan 200--
-Write-Host "Configuring VLAN 200..."
-Add-DhcpServerV4Scope -Name "Vlan 200" -StartRange 172.18.0.2 -EndRange 172.18.0.254 -SubnetMask 255.255.255.0 -Type Both
-
+$j += Start-Job -Name CreateScope -ScriptBlock {
+    Add-DhcpServerV4Scope -Name "Vlan 200" -StartRange 172.18.0.2 -EndRange 172.18.0.254 -SubnetMask 255.255.255.0 -Type Both
+}
+Wait-Job -Name CreateScope
 # DNS, Router, Default Gateway en mogelijk andere zaken toevoegen
-
-Set-DhcpServerV4OptionValue -DnsServer 172.18.1.66, 172.18.1.67 -DnsDomain "red.local" -Router 172.18.0.1 -ScopeId 172.18.0.0 -Force
-Set-DhcpServerV4OptionValue -OptionId 066 -Value 172.18.1.6 # Value kan mogelijks nog veranderen
-Set-DhcpServerV4OptionValue -OptionId 067 -Value "\smsboot\x64\wdsnbp.com" # Value kan mogelijks nog veranderen
+$j += Start-Job -Name SettingOptionValues -ScriptBlock {
+    Set-DhcpServerV4OptionValue -DnsServer 172.18.1.66, 172.18.1.67 -DnsDomain "red.local" -Router 172.18.0.1 -ScopeId 172.18.0.0 -Force
+    Set-DhcpServerV4OptionValue -OptionId 066 -Value 172.18.1.6 # Value kan mogelijks nog veranderen
+    Set-DhcpServerV4OptionValue -OptionId 067 -Value "\smsboot\x64\wdsnbp.com" # Value kan mogelijks nog veranderen
+}
 # Lease time configureren
-
-Set-DhcpServerv4Scope -ScopeId 172.18.0.0 -LeaseDuration (New-TimeSpan -Days 2)
-
-Write-Host "VLAN 200 configured"
-Write-Host "Authorizing DHCP server..."
+$j += Start-Job -Name SettingLease -ScriptBlock {
+    Set-DhcpServerv4Scope -ScopeId 172.18.0.0 -LeaseDuration (New-TimeSpan -Days 2)
+}
 
 $password = "vagrant" | ConvertTo-SecureString -AsPlainText -Force
 $username = "RED\Administrator"
 $credential = New-Object System.Management.Automation.PSCredential($username, $password)
 
-Start-Job -Name AuthorizeDHCP -Credential $credential -ScriptBlock {
+$j += Start-Job -Name AuthorizeDHCP -Credential $credential -ScriptBlock {
     Add-DhcpServerInDC -DnsName "Kilo2.red.local"
 }
 
-Wait-Job -Name AuthorizeDHCP
+Wait-Job -Job $j
+Receive-Job -Job $j
 
 # Restart DHCP Server
 Restart-service dhcpserver
