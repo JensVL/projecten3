a68c1c30d77a8273f5946f09f7dfcf2f2563d5a7
Arno Van Nieuwenhove <arno.vannieuwenhove@student.hogent.be>
2019-10-29 17:26:17 +0100

    Create 2_InstallDCDNS.ps1

--- Servers/Bravo2/Testomgeving/Scripts/2_InstallDCDNS.ps1
+++ Servers/Bravo2/Testomgeving/Scripts/2_InstallDCDNS.ps1
@@ -5,19 +5,8 @@ $IpAddress = "172.18.1.67"
 $IpAlfa2 = "172.18.1.66"
 $CIDR = "27"
 $default_gateway  = "172.18.1.65"
-# $DefaultGateWay = "172.18.1.98"
 $AdapterNaam = "LAN"
 $DSRM = ConvertTo-SecureString "Admin2019" -asPlainText -force
-# $DebugPreference = "Continue"
-# $VerbosePreference = "Continue"
-# $InformationPreference = "Continue"
-
-# LOG SCRIPT TO FILE (+ op het einde van het script Stop-Transcript doen):
-Start-Transcript "C:\ScriptLogs\2_InstallDCDNSlog.txt"
-
-Write-host "Waiting 15 seconds before executing script" -ForeGroundColor "Green"
-start-sleep -s 15 #Zorgen dat de server genoeg tijd heeft door 15 seconden te laten wachten.
-Write-host "Starting script now:" -ForeGroundColor "Green"
 
 # 1) Stel Datum/tijd correct in:
 # Romance standard time = Brusselse tijd
@@ -25,18 +14,13 @@ Write-host "Setting correct timezone and time format settings:" -ForeGroundColor
 Set-Culture -CultureInfo $Land
 set-timezone -Name "Romance Standard Time"
 
-# 2) Firewall uitschakelen
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-
 # 3) Zorgen voor juist LAN adapter. Via intern netwerk.
 Write-host "Changing NIC adapter names:" -ForeGroundColor "Green"
-Get-NetAdapter -Name "Ethernet" | Rename-NetAdapter -NewName $AdapterNaam
-
-# 4) LAN adapter instellen
-Write-host "Setting correct ipv4 settings:" -ForeGroundColor "Green"
-New-NetIPAddress -InterfaceAlias "$AdapterNaam" -IPAddress "$IpAddress" -PrefixLength $CIDR -DefaultGateWay $default_gateway
+Get-NetAdapter -Name "Ethernet 2" | Rename-NetAdapter -NewName $AdapterNaam
 
 # 4) Prefixlength = CIDR notatie van subnet (in ons geval 255.255.255.224)
+Write-host "Setting correct ipv4 settings:" -ForeGroundColor "Green"
+
 $existing_ip=(Get-NetAdapter -Name $AdapterNaam | Get-NetIPAddress -AddressFamily IPv4).IPAddress
 if("$existing_ip" -ne "$IpAddress") {
     Write-host "Setting correct ipv4 settings:" -ForeGroundColor "Green"
@@ -47,8 +31,6 @@ if("$existing_ip" -ne "$IpAddress") {
 #Disable-NetAdapter -Name "Ethernet" -Confirm:$false
 
 # 6) DNS van LAN van Alfa2 instellen op Hogent DNS servers:
-# Eventueel commenten tijdens testen in demo omgeving
-# Set-DnsClientServerAddress -InterfaceAlias "$AdapterNaam" -ServerAddress "$IpAlfa2","$IpAddress"
 Set-DnsClientServerAddress -InterfaceAlias "$AdapterNaam" -ServerAddress "$IpAlfa2","$IpAddress"
 
 # 7) Configure Administrator account
@@ -59,25 +41,16 @@ Write-host "Starting installation of ADDS role:" -ForeGroundColor "Green"
 Install-WindowsFeature AD-domain-services -IncludeManagementTools
 import-module ADDSDeployment
 
-# 6) Idem aan het 1_RUNFIRST.ps1 script zal deze registry instelling ervoor zorgen dat ons volgende script automatisch wordt geladen
-# Want het installeren van de ADDS role herstart automatisch onze server
-# RunOnce verwijderd deze instelling automatisch nadat het script klaar is met runnen
-# Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name ResumeScript `
-                # -Value "C:\Windows\system32\WindowsPowerShell\v1.0\Powershell.exe -executionpolicy bypass -file `"$VBOXdrive\3_ConfigDCDNS.ps1`""
 
 # 9) DSRM instellen
 $creds = New-Object System.Management.Automation.PSCredential ("RED\Administrator", (ConvertTo-SecureString "Admin2019" -AsPlainText -Force))
 
-# 7.1) De eerste command zorgt ervoor dat je je als admin niet steeds moet inloggen wanneer je wijzigingen wil doen:
-# set-itemproperty -Path "REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
-#                 -name "ConsentPromptBehaviorAdmin" -value 0
-# Set-ItemProperty -Path "REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
-#                 -Name "FilterAdministratorToken" -value 1
-
 
 # 10) Joinen van domein "red.local":
-Write-host "Starting configuration of red.local domain:" -ForeGroundColor "Green"
-install-ADDSDomainController -DomainName "red.local" `
+Write-host "Domain joinen:" -ForeGroundColor "Green"
+
+install-ADDSDomainController 
+                  -DomainName "red.local" `
                   -ReplicationSourceDC "Alfa2.red.local" `
                   -credential $creds `
                   -installDns:$true `
@@ -85,6 +58,3 @@ install-ADDSDomainController -DomainName "red.local" `
                   -NoRebootOnCompletion:$true `
                   -SafeModeAdministratorPassword $DSRM `
                   -force:$true
-
-
-Stop-Transcript
