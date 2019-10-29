b31364efdc0f4f7337b2a7654ee5db9ae523b281
LAURENS BLANCQUAERTCASSAER <laurens.blancquaertcassaer@student.hogent.be>
2019-10-29 09:51:31 +0100

    Import Alfa2 to Papa2 branch

--- Servers/Alfa2/Testomgeving/Scripts/3_ConfigDCDNS.ps1
+++ Servers/Alfa2/Testomgeving/Scripts/3_ConfigDCDNS.ps1
@@ -1,8 +1,124 @@
+#------------------------------------------------------------------------------
+# Description
+#------------------------------------------------------------------------------
+# Installatiescript dat de configuratie doet van de DNS
+#   (primary forward en reversed lookup zones maken + Forwarder)
 
-# VARIABLES:
+#------------------------------------------------------------------------------
+# Variables
+#------------------------------------------------------------------------------
+param(
+    [string]$Bravo2IP    = "172.18.1.67", # DC2 / DNS2
+    [string]$Charlie2IP  = "172.18.1.68", # Exchange Server
+    [string]$Delta2IP    = "172.18.1.69", # IIS Webserver
+    [string]$Kilo2IP     = "172.18.1.1", # DHCP Server
+    [string]$Lima2IP     = "172.18.1.2", # File Server
+    [string]$Mike2IP     = "172.18.1.3", # Intranet Sharepoint Server
+    [string]$November2IP = "172.18.1.4", # SQL Server
+    [string]$Oscar2IP    = "172.18.1.5", # Monitoring Server
+    [string]$Papa2IP     = "172.18.1.6", # SCCM Server
+    [string]$wan_adapter_name = "NAT"
+)
 
+#------------------------------------------------------------------------------
+# Wait for AD services to become available
+#------------------------------------------------------------------------------
+while ($true) {
+    try {
+        Get-ADDomain | Out-Null
+        break
+    } catch {
+        Start-Sleep -Seconds 10
+    }
+}
 
-# LOG SCRIPT TO FILE (+ op het einde van het script Stop-Transcript doen):
-Start-Transcript Start-Transcript "C:\ScriptLogs\3_ConfigDCDNSlog.txt"
+#------------------------------------------------------------------------------
+# Configure DNS
+#------------------------------------------------------------------------------
+# Fix adapters
+Set-DnsClientServerAddress -InterfaceAlias $wan_adapter_name -ResetServerAddresses
 
-Stop-Transcript
+# Stel forward primary lookup zones in voor alle servers in het red domein:
+Write-host "Setting DNS primary zone for red.local" -ForeGroundColor "Green"
+Set-DnsServerPrimaryZone -Name "red.local" -SecureSecondaries "TransferToZoneNameServer"
+
+# Voeg de servers als AAAA records toe met hun ip adres
+#   in de aangemaakte primary zone: (name to ip address)
+# NOTE: Charlie2 Exchange (mail) server = MX record ipv AAAA record
+#   en Bravo2 DC2 = NS record (NS record niet zelf aanmaken gebeurd
+#   automatisch volgens AD-integration zones)
+# NOTE: MX record -MailExchange option moet pointen naar
+#   bestaande A record
+#   (Zie -Mail Exchange optie Microsoft docs Add-DnsServerResourceRecordMX)
+Write-host "Adding DNS A and MX records for the servers of red.local" -ForeGroundColor "Green"
+
+function add_dns_record() {
+    param(
+        [string]$record_name,
+        [string]$record_zone_name,
+        [string]$record_type,
+        [string]$ipaddress,
+        # [string]$record_mx_name,
+        # [string]$record_cname_name,
+        [string]$record_mail_exchange,
+        [int]$record_preference,
+        [string]$record_hostname_alias
+    )
+
+    # TODO: test with and without "2> $null"
+    $record_exists=(Get-DnsServerResourceRecord -Name $record_name -RRType $record_type -ZoneName $record_zone_name 2> $null)
+
+    if(!$record_exists) {
+        # make record
+        if($record_type -eq "MX") {
+            Add-DnsServerResourceRecordMX -Name $record_name -ZoneName $record_zone_name -MailExchange $record_mail_exchange -Preference $record_preference
+        }
+        elseif ($record_type -eq "CNAME") {
+            Add-DnsServerResourceRecordCName -Name $record_name -ZoneName $record_zone_name -HostNameAlias $record_hostname_alias
+        }
+        elseif ($record_type -eq "A") {
+            Add-DnsServerResourceRecordA -Name $record_name -ZoneName $record_zone_name -IPv4Address $ipaddress
+        }
+    }
+    else {
+        Write-Warning "Record $record_name already exists (skipping)"
+    }
+
+}
+
+add_dns_record -record_name "mail" -record_zone_name "red.local" -record_type "A" -ipaddress $Charlie2IP
+add_dns_record -record_name "mail" -record_zone_name "red.local" -record_type "MX" -record_mail_exchange "mail.red.local" -record_preference 100
+add_dns_record -record_name "owa" -record_zone_name "red.local" -record_type "CNAME" -record_hostname_alias "mail.red.local"
+
+add_dns_record -record_name "Delta2" -record_zone_name "red.local" -record_type "A" -ipaddress $Delta2IP
+add_dns_record -record_name "Kilo2" -record_zone_name "red.local" -record_type "A" -ipaddress $Kilo2IP
+add_dns_record -record_name "Lima2" -record_zone_name "red.local" -record_type "A" -ipaddress $Lima2IP
+add_dns_record -record_name "Mike2" -record_zone_name "red.local" -record_type "A" -ipaddress $Mike2IP
+add_dns_record -record_name "November2" -record_zone_name "red.local" -record_type "A" -ipaddress $November2IP
+add_dns_record -record_name "Oscar2" -record_zone_name "red.local" -record_type "A" -ipaddress $Oscar2IP
+add_dns_record -record_name "Papa2" -record_zone_name "red.local" -record_type "A" -ipaddress $Papa2IP
+
+# Add-DnsServerResourceRecordA -Name "mail" -ZoneName "red.local" -IPv4Address "$Charlie2IP"
+# Add-DnsServerResourceRecordMX -Name "mail" -MailExchange "mail.red.local" -ZoneName "red.local" -Preference 100
+# Add-DnsServerResourceRecordCName -Name "owa" -HostNameAlias "mail.red.local" -ZoneName "red.local"
+
+# Add-DnsServerResourceRecordA -Name "Delta2" -ZoneName "red.local" -IPv4Address "$Delta2IP"
+# Add-DnsServerResourceRecordA -Name "Kilo2" -ZoneName "red.local" -IPv4Address "$Kilo2IP"
+# Add-DnsServerResourceRecordA -Name "Lima2" -ZoneName "red.local" -IPv4Address "$Lima2IP"
+# Add-DnsServerResourceRecordA -Name "Mike2" -ZoneName "red.local" -IPv4Address "$Mike2IP"
+# Add-DnsServerResourceRecordA -Name "November2" -ZoneName "red.local" -IPv4Address "$November2IP"
+# Add-DnsServerResourceRecordA -Name "Oscar2" -ZoneName "red.local" -IPv4Address "$Oscar2IP"
+# Add-DnsServerResourceRecordA -Name "Papa2" -ZoneName "red.local" -IPv4Address "$Papa2IP"
+
+# Firewall uitzetten (want we gebruiken hardware firewall):
+ Write-Host "Turning firewall off:" -ForeGroundColor "Green"
+ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+# DNS forwarder instellen op Hogent DNS servers:
+Add-DnsServerForwarder -IPAddress 193.190.173.1,193.190.173.2
+
+# 5) Start het 4_ADstructure.ps1 script als Administrator:
+# Write-host "Running next script 4_ADSTRUCTURE.ps1 as admin:" -ForeGroundColor "Green"
+# Start-Process powershell -Verb runAs -ArgumentList "$VBOXdrive\4_ADstructure.ps1"
+
+# Stop-Transcript
--- Servers/Alfa2/Testomgeving/Scripts/999_PrepareADforSCCM.ps1
+++ Servers/Alfa2/Testomgeving/Scripts/999_PrepareADforSCCM.ps1
@@ -1,34 +1,42 @@
-# Dit script moet uitgevoerd worden na de installatie van beide DC'S en nadat Papa2 (SCCM Deployment Server) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! NIET VERGETEN UIT TE VOEREN NADAT PAPA2 IN DOMEIN ZIT EN VOOR SCCM INSTALLATIe!
+﻿# Dit script moet uitgevoerd worden na de installatie van beide DC'S en nadat Papa2 (SCCM Deployment Server) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! NIET VERGETEN UIT TE VOEREN NADAT PAPA2 IN DOMEIN ZIT EN VOOR SCCM INSTALLATIe!
 # aan het domein is toegevoegd Het script zal de AD voorbereiden op de installatie van SCCM
 # en ervoor zorgen dat alles goed werkt
 # Elke stap wordt uitgelegd met zijn eigen comment
 
 ############################################## BELANGRIJK #####################################################################################
-# VOER DIT SCRIPT UIT ALS ADMINISTRATOR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# VOER DIT SCRIPT UIT ALS ADMINISTRATOR ACCOUNT TIJDENS DE DEMO
 ############################################## BELANGRIJK #####################################################################################
 
 # VARIABLES:
-$VBOXdrive = "Z:\"
+$VBOXdrive = "C:\Vagrant"
+
+$SCCMAdmin = "SCCMadmin"
+
+# PREFERENCE VARIABLES: (Om Debug,Verbose en informaation info in de Start-Transcript log files te zien)
+$DebugPreference = "Continue"
+$VerbosePreference = "Continue"
+$InformationPreference = "Continue"
 
 # LOG SCRIPT TO FILE (+ op het einde van het script Stop-Transcript doen):
 Start-Transcript "C:\ScriptLogs\999_PrepareADforSCCMlog.txt"
 
 # 1) Maakt een admin account aan voor SCCM (SCCMAdmin) en maak hem lid van de Domain Admin group:
-# 1.1) Met het Get-Credential command openen we opnieuw een prompt zodat het wachtwoord van deze account
-# kan ingegeven worden (password wordt opgeslaan in $SCCMPassword en wordt daarna converted naar een secure string)
-$CurrentCredentials = Get-Credential -UserName $env:USERNAME -Message "Credentials required for SCCMAdmin account"
-$SCCMPassword = $CurrentCredentials.GetNetworkCredential().Password
-$SecureStringPwd = ConvertTo-SecureString "$SCCMPassword" -AsPlainText -Force
+# 1.1) Password instellen op Admin2019
+$password=ConvertTo-SecureString "Admin2019" -asPlainText -force
 
 # 1.2) Het aanmaken van de SCCM account zelf:
-Write-Host "Creating SCCMAdmin account in the AD for the Papa2 server:"
-New-ADUser -GivenName "SCCM" -Surname "Admin" -Name "SCCMAdmin" -PasswordNeverExpires $true -AccountPassword $SecureStringPwd
-set-adUser -Enabled $true -Identity "SCCMAdmin"
+Write-Host "Creating SCCMAdmin account in the AD for the Papa2 server:" -ForeGroundColor "Green"
+New-ADUser -GivenName "SCCM" -Surname "Admin" -Name "$SCCMAdmin" -PasswordNeverExpires $true -AccountPassword $password
+New-ADComputer -Name "Papa2"
+set-adUser -Enabled $true -Identity "$SCCMAdmin"
 
 # 1.3) Voeg nu de SCCM acocunt toe aan de Domain Admin en Administrators groups zodat hij full domain admin rechten heeft:
-Write-host "Adding SCCMAdmin to Domain Admin en Administrators group:"
-Add-ADGroupMember -Identity "Domain Admins" -Members "SCCMAdmin"
-Add-ADGroupMember -Identity "Administrators" -Members "SCCMAdmin"
+Write-host "Adding SCCMAdmin to Domain Admin en Administrators group:" -ForeGroundColor "Green"
+Add-ADGroupMember -Identity "Domain Admins" -Members "$SCCMAdmin"
+Add-ADGroupMember -Identity "Administrators" -Members "$SCCMAdmin"
+Add-ADGroupMember -Identity "Domain Admins" -Members "Vagrant"
+Add-ADGroupMember -Identity "Schema Admins" -Members "Vagrant"
+Add-ADGroupMember -Identity "Administrators" -Members "Vagrant"
 
 # 2) Nu moeten we in ADSIedit (Waar al de instellingen van ADDS staan) een System Management container maken onder de "System"
 # 2.1) Verbind met ADSIedit:
@@ -36,7 +44,7 @@ Add-ADGroupMember -Identity "Administrators" -Members "SCCMAdmin"
 $ASDIconnection = [ADSI]"LDAP://localhost:389/cn=System,dc=red,dc=local"
 
 # 2.2) Maak de System Management container aan in ADSIedit:
-Write-Host "Creating System Management container in ADSIedit:"
+Write-Host "Creating System Management container in ADSIedit:" -ForeGroundColor "Green"
 $SysManContainer = $ASDIconnection.Create("container", "cn=System Management")
 $SysManContainer.SetInfo()
 
@@ -47,10 +55,10 @@ $SysManContainer.SetInfo()
 $SystemManagementCN = [ADSI]("LDAP://localhost:389/cn=System Management,cn=System,dc=red,dc=local")
 
 # 3.2) Get Papa2 als computer object:
-$server = get-adcomputer "Papa2"
+$SCCMserver = get-adcomputer "Papa2"
 
 # 3.3) Get identity reference van het computer object:
-$SID = [System.Security.Principal.SecurityIdentifier] $server.SID
+$SID = [System.Security.Principal.SecurityIdentifier] $SCCMserver.SID
 $ServerIdentity = [System.Security.Principal.IdentityReference] $SID
 
 # 3.4) Stel permissions in als ALLOW full control (full control = GenericAll hieronder):
@@ -62,7 +70,7 @@ $inheritanceAll = [System.DirectoryServices.ActiveDirectorySecurityInheritance] 
 
 # 3.5) Dit zal de gekozen permissies toepassen op de System Management container:
 # in ADSIedit noemt een permission rule "Access rule" (AddAccessRule methode)
-Write-Host "Setting permissions of System Management container to Papa2 server"
+Write-Host "Setting permissions of System Management container to Papa2 server" -ForeGroundColor "Green"
 $PermissionsRule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
 $ServerIdentity,$permissions,$allow,$inheritanceAll
 
@@ -76,18 +84,17 @@ $SystemManagementCN.psbase.commitchanges()
 
 #4.1) Eerst de folder "ExtendADSchema" uit de VB share folder lokaal kopiëren naar Alfa2 server:
 # Dit moet omdat je anders een warning "This file might be unsafe" krijgt.
-Copy-Item "$VBOXdrive\ExtendADschema" -Destination "C:\Users\Administrator.red\Desktop" -Recurse
+Copy-Item "$VBOXdrive\ExtendADschema" -Destination "C:\Users\Administrator\Desktop" -Recurse
 
 # 4.2) Dan voer je het script (ALS ADMINISTRATOR!) uit om de AD schema te extenden:
-Write-Host "Extending AD Schema:"
-Start-Process -Verb RunAs C:\Users\Administrator.red\Desktop\ExtendADschema\extadsch.exe
+Write-Host "Extending AD Schema:" -ForeGroundColor "Green"
+Start-Process "C:\Users\Administrator\Desktop\ExtendADschema\extadsch.exe"
 
 # NOG STEEDS FAILED TO EXTEND AD SCHEMA ERROR Error 8224?
 # Oplossing was: Tweede DomainController stond niet aan. Deze moet aanstaan (voor replication redenen)
 # C:\ExtADSch.log check deze log file om te zien of extenden van het AD schema succesvol was
 
-Remove-Item -Path "C:\Users\Administrator.red\Desktop\ExtendADschema" -Force
 # Verplaats bovengenoemde log naar desktop van domain admin:
-Move-item -Path "C:\ExtADSch.log" -Destination "C:\Users\Administrator.red\Desktop"
+Move-item -Path "C:\ExtADSch.log" -Destination "C:\Users\Administrator\Desktop"
 
 Stop-Transcript
