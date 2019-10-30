23d60e2d92ade75180a8f0de500db53399a7acb3
Tibo Vanhercke <tibo.vanhercke@student.hogent.be>
2019-10-30 21:19:49 +0100

    Added DNS and DG Option Values check and formalised output

--- Servers/Kilo2/testen/test_script.ps1
+++ Servers/Kilo2/testen/test_script.ps1
@@ -1,41 +1,29 @@
-﻿$InterfaceIndex = Get-NetIPAddress 172.18.1.1 | select -ExpandProperty InterfaceIndex
-Write-Host 'Check IP Address:'
-Write-Host (Get-NetIPAddress -InterfaceIndex $InterfaceIndex | select -ExpandProperty IPAddress)
-Write-Host
-Write-Host 'Check Default Gateway:'
-Write-Host (Get-NetRoute -InterfaceIndex $InterfaceIndex | select -ExpandProperty NextHop)
-Write-Host
-Write-Host 'Check DNS Server Addresses:'
-Write-Host (Get-DnsClientServerAddress -InterfaceIndex $InterfaceIndex | select -ExpandProperty ServerAddresses)
-Write-Host
-Write-Host 'Check Domain Name:'
-$env:USERDNSDOMAIN
-Write-Host
-Write-Host 'Check if DHCP is installed:'
-$DHCP = Get-WindowsFeature -Name DHCP 
-$DHCPInstalled = $DHCP | select -ExpandProperty InstallState
-$DHCPName = $DHCP | select -ExpandProperty Name
-Write-Host "$DHCPName : $DHCPInstalled"
-Write-Host
-Write-Host 'Check security groups:'
-Write-Host (Get-WmiObject win32_group -Filter "name='DHCP Users'" | select -ExpandProperty name)
-Write-Host (Get-WmiObject win32_group -Filter "name='DHCP Administrators'" | select -ExpandProperty name)
-Write-Host
-Write-Host 'Check if scope is correct:'
-$Scope = Get-DhcpServerV4Scope
-$ScopeId = $Scope | select -ExpandProperty ScopeId
-$Name = $Scope | select -ExpandProperty Name
-$StartRange = $Scope | select -ExpandProperty StartRange
-$EndRange = $Scope | select -ExpandProperty EndRange
-$LeaseDuration = $Scope | select -ExpandProperty LeaseDuration
-Write-Host "ScopeId: $ScopeId"
-Write-Host "Name: $Name"
-Write-Host "StartRange: $StartRange"
-Write-Host "EndRange: $EndRange"
-Write-Host "LeaseDuration: $LeaseDuration"
-Write-Host
-Write-Host 'Check if correct options are configured:'
-Get-DhcpServerV4OptionValue
-Write-Host
-Write-Host 'Check if server is authorized:'
-Get-DhcpServerInDC
+﻿$password = "Admin2019" | ConvertTo-SecureString -AsPlainText -Force
+$username = "RED\Administrator"
+$credential = New-Object System.Management.Automation.PSCredential($username, $password)
+Start-Job -Name Test -Credential $credential -ScriptBlock {
+    $DHCP = Get-WindowsFeature -Name DHCP 
+    Write-Host 'Check name and domain:'
+    Get-WmiObject Win32_ComputerSystem | Select-Object -Property Name, Domain | Format-List | Out-String    
+    Write-Host 'Check IP Address:'
+    Get-NetIPAddress -AddressFamily IPv4 | Select-Object -Property InterfaceAlias, IPAddress, PrefixLength | Format-List | Out-String
+    Write-Host 'Check Default Gateway:'
+    Get-NetRoute -AddressFamily IPv4 -DestinationPrefix "0.0.0.0/0" | Select-Object -Property InterfaceAlias, NextHop | Format-List | Out-String
+    Write-Host 'Check DNS Server Addresses:'
+    Get-DnsClientServerAddress -AddressFamily IPv4 | Select-Object -Property InterfaceAlias, ServerAddresses | Format-List | Out-String
+    Write-Host 'Check if DHCP is installed:'
+    $DHCP | Select-Object -Property Name, InstallState | Format-List | Out-String
+    Write-Host 'Check security groups:'
+    Get-WmiObject win32_group -Filter "name LIKE 'DHCP%'" | Select-Object -Property Name | Format-List | Out-String
+    Write-Host 'Check if scope is correct:'
+    $Scope = Get-DhcpServerV4Scope | Select-Object -Property ScopeID, Name, State, SubnetMask, StartRange, EndRange, LeaseDuration, Type
+    $Scope | Format-List | Out-String
+    Write-Host 'Check if correct options are configured:'
+    $Options = Get-DhcpServerv4OptionValue -ScopeId 172.18.0.0 | select -Property OptionID, Name, Value
+    $Options += (Get-DhcpServerv4OptionValue -OptionId 66,67 | Select-Object -Property OptionID, Name, Value)
+    $Options | Sort-Object -Property OptionID | Format-List | Out-String
+    Write-Host 'Check if server is authorized:'
+    Get-DhcpServerInDC | Select-Object -Property DnsName, IPAddress | Format-List | Out-String
+}
+Wait-Job -Name Test
+Receive-Job -Name Test
