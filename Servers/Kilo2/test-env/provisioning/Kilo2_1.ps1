77f16f10e5e451616a4b3069535e2fd13aac9b7a
Tibo Vanhercke <tibo.vanhercke@student.hogent.be>
2019-10-30 21:29:23 +0100

    Edited notifications

--- Servers/Kilo2/test-env/provisioning/Kilo2_1.ps1
+++ Servers/Kilo2/test-env/provisioning/Kilo2_1.ps1
@@ -14,28 +14,27 @@ $InterfaceIndex = Get-NetIPAddress -IPAddress $IP | select -ExpandProperty Inter
 # IP Configuraties
 
 # DHCP rol installeren
+Write-Host ">>> Starting installation DHCP in background"
 Start-Job -Name InstallDHCP -ScriptBlock {
     Install-WindowsFeature -Name DHCP -IncludeManagementTools
 }
 
 # Configuring network adapters
-Write-Host "Configuring IP..."
+Write-Host ">>> Configuring IP"
 Remove-NetIPAddress -InterfaceIndex $InterfaceIndex -Confirm:$false
 Remove-NetRoute -InterfaceIndex $InterfaceIndex -Confirm:$false
 Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ResetServerAddresses
 New-NetIPAddress -InterfaceIndex $InterfaceIndex -IPAddress $IP -PrefixLength 24 -DefaultGateway $DefaultGateway
 Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses $DNS
-Write-Host "Ip Configured"
 
 # Add delay to give the server time to adapt to new ip configurations
 # If no delay is added the Machine will not be adapted to the new ip configurations and will fail to join the domain
 
 Start-Sleep -s 5
 # Toevoegen aan domain
-Write-Host "Adding server to Domain..."
+Write-Host ">>> Adding server to Domain"
 
 Add-Computer -DomainName $domain -Credential $credential -Force
-Write-Host "Server added to Domain"
 
 Wait-Job -Name InstallDHCP
 Receive-Job -Name InstallDHCP
--- Servers/Kilo2/test-env/provisioning/Kilo2_2.ps1
+++ Servers/Kilo2/test-env/provisioning/Kilo2_2.ps1
@@ -16,33 +16,29 @@ $ScopeDG = "172.18.0.1"
 $DHCPOption66Value = "papa2.$Domain"
 
 # Creating security groups
-Write-Host "Creating security groups..."
+Write-Host ">>> Creating security groups"
 cmd.exe /c "netsh dhcp add securitygroups"
-Write-Host "Security Groups created"
 # Configureren van de Scopes op de DHCP Server
 
+Write-Host ">>> Authorizing DHCP Server in background"
 Start-Job -Name AuthorizeDHCP -Credential $credential -ScriptBlock {
-    Write-Host "Authorizing DHCP Server..."
+    Write-Host ">>> Authorizing DHCP"
     Add-DhcpServerInDC
-    Write-Host "DHCP Server Authorized"
 }
 
 # --scope vlan 200--
-Write-Host "Creating DHCP Scope..."
+Write-Host ">>> Creating DHCP Scope"
 Add-DhcpServerV4Scope -Name "Vlan 200" -StartRange $StartRange -EndRange $StopRange -SubnetMask $SubnetMask -Type Both
-Write-Host "DHCP Scope Created"
 # DNS, Router, Default Gateway en mogelijk andere zaken toevoegen
 
-Write-Host "Configuring Option Values..."
+Write-Host ">>> Configuring Option Values"
 Set-DhcpServerV4OptionValue -DnsServer $DHCPDnsServers -DnsDomain $Domain -Router $ScopeDG -ScopeId $ScopeID -Force
 Set-DhcpServerV4OptionValue -OptionId 066 -Value $DHCPOption66Value # Value kan mogelijks nog veranderen
 Set-DhcpServerV4OptionValue -OptionId 067 -Value "\smsboot\x64\wdsnbp.com" # Value kan mogelijks nog veranderen
-Write-Host "Option Values configured"
 # Lease time configureren
 
-Write-Host "Configuring Lease Duration..."
+Write-Host ">>> Configuring Lease Duration"
 Set-DhcpServerv4Scope -ScopeId $ScopeID -LeaseDuration (New-TimeSpan -Days 2)
-Write-Host "Lease Duration Configured"
 
 Wait-Job -Name AuthorizeDHCP
 Receive-Job -Name AuthorizeDHCP
