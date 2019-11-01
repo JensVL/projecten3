1e45b38d503572bb497fa9bde65fe2d0854979d2
cevde <32325922+cevde@users.noreply.github.com>
2019-11-01 15:51:37 +0100

    Update 3_SQL.ps1

--- Servers/November2/Scripts/3_SQL.ps1
+++ Servers/November2/Scripts/3_SQL.ps1
@@ -3,7 +3,7 @@ Write-Host 'Installatie SQL Server 2017'
 $folderpath="C:\Users\Administrator\Desktop"
 $filepath="$folderpath\Setup.exe"
 $Parms = " /qs /Install /ConfigurationFile=ConfigurationFile.ini -silent"
-Setup.exe /ConfigurationFile=ConfigurationFile.ini
+.\Setup.exe /ConfigurationFile=ConfigurationFile.ini
 
 Write-Host 'Done'
 Restart-Computer
