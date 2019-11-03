023c12fb91a782b546926ae93a6e24151961629c
Tibo Vanhercke <tibo.vanhercke@student.hogent.be>
2019-11-03 18:25:44 +0100

    Changed notifications, edited comments, edited code

--- Servers/Kilo2/test-env/provisioning/Kilo2_1.ps1
+++ Servers/Kilo2/test-env/provisioning/Kilo2_1.ps1
@@ -5,7 +5,7 @@ param(
     [string]$primary_dns    = "172.18.1.66",
     [string]$secondary_dns  = "172.18.1.67",
     [string]$domain         = "red.local",
-    [string]$domain_user    = "RED\Administrator",
+    [string]$domain_user    = "Administrator",
     [string]$domain_pw      = "Admin2019"
 )
 
@@ -17,38 +17,32 @@ Start-Job -Name InstallDHCP -ScriptBlock {
     Install-WindowsFeature -Name DHCP -IncludeManagementTools
 }
 
-Start-Job -Name ConfiguringIP -ScriptBlock {
-    Write-Host "Configuring IP..."
-    # $IP = "172.18.1.1"
-    # $DefaultGateway = "172.18.1.66"
-    # $DNS = "172.18.1.66"
-    $InterfaceIndex = Get-NetIPAddress -IPAddress $IP | select -ExpandProperty InterfaceIndex
+Write-Host ">>> Configuring IP"
+# $IP = "172.18.1.1"
+# $DefaultGateway = "172.18.1.66"
+# $DNS = "172.18.1.66"
+$InterfaceIndex = Get-NetIPAddress -IPAddress $IP | Select-Object -ExpandProperty InterfaceIndex
 
-    Remove-NetIPAddress -InterfaceIndex $InterfaceIndex -Confirm:$false
-    Remove-NetRoute -InterfaceIndex $InterfaceIndex -Confirm:$false
+Remove-NetIPAddress -InterfaceIndex $InterfaceIndex -Confirm:$false
+Remove-NetRoute -InterfaceIndex $InterfaceIndex -Confirm:$false
 
-    Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ResetServerAddresses
-    New-NetIPAddress -InterfaceIndex $InterfaceIndex -IPAddress $IP -PrefixLength $lan_prefix -DefaultGateway $DefaultGateway
-    Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses($primary_dns, $secondary_dns)
-    Start-Sleep -s 5
-    Write-Host "Ip Configured"
-}
+Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ResetServerAddresses
+New-NetIPAddress -InterfaceIndex $InterfaceIndex -IPAddress $IP -PrefixLength $lan_prefix -DefaultGateway $DefaultGateway
+Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses($primary_dns, $secondary_dns)
 
 # Add delay to give the server time to adapt to new ip configurations
 # If no delay is added the Machine will not be adapted to the new ip configurations and will fail to join the domain
 
 Start-Sleep -s 5
 # Toevoegen aan domain
-Start-Job -Name AddToDomain -ScriptBlock {
-    Write-Host "Adding server to Domain..."
-    # $domain = "red.local"
-    # $password = $domain_pw | ConvertTo-SecureString -AsPlainText -Force
-    # $username = "Administrator"
-    $credential = New-Object System.Management.Automation.PSCredential($domain_user, ($domain_pw | ConvertTo-SecureString -AsPlainText -Force))
 
-    Add-Computer -DomainName $domain -DomainCredential $credential -Force
-    Write-Host "Server added to Domain"
-}
+Write-Host ">>> Adding server to Domain"
+# $domain = "red.local"
+# $password = $domain_pw | ConvertTo-SecureString -AsPlainText -Force
+# $username = "Administrator"
+$credential = New-Object System.Management.Automation.PSCredential("RED\$domain_user", ($domain_pw | ConvertTo-SecureString -AsPlainText -Force))
 
-Wait-Job
-Receive-Job -Name KeyboardSetting, ConfiguringIP, AddToDomain, InstallDHCP
+Add-Computer -DomainName $domain -DomainCredential $credential -Force
+
+
+Wait-Job -Name InstallDHCP | Receive-Job
