93987cad453cdc12feea5f7c7aca41693bb79799
AronMarckx <aronmarckx@hotmail.com>
2019-11-04 16:11:58 +0100

    Update 3_SQL.ps1

--- Release/Scripts_ESXI/November2/3_SQL.ps1
+++ Release/Scripts_ESXI/November2/3_SQL.ps1
@@ -4,7 +4,7 @@ Add-LocalGroupMember -Group "Administrators" -Member "RED\Papa2$"
 
 Write-Host 'Installatie SQL Server 2017'
 #Installatie SQL Server 2017
-$folderpath="C:\Scripts_ESXI"
+$folderpath="C:\Scripts_ESXI\November2"
 $filepath="$folderpath\Setup.exe"
 $Parms = " /qs /Install /ConfigurationFile=ConfigurationFile.ini -silent"
 .\Setup.exe /ConfigurationFile=ConfigurationFile.ini
