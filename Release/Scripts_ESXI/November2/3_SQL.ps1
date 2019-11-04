9a8d9d0fac0fc3fe6f7eadb889085896f34d964c
AronMarckx <aronmarckx@hotmail.com>
2019-11-04 15:57:16 +0100

    Add files via upload

---
+++ Release/Scripts_ESXI/November2/3_SQL.ps1
@@ -0,0 +1,13 @@
+#Member voor Papa2 toevoegen
+Add-LocalGroupMember -Group "Administrators" -Member "RED\Administrator"
+Add-LocalGroupMember -Group "Administrators" -Member "RED\Papa2$"
+
+Write-Host 'Installatie SQL Server 2017'
+#Installatie SQL Server 2017
+$folderpath="C:\Users\Administrator\Desktop"
+$filepath="$folderpath\Setup.exe"
+$Parms = " /qs /Install /ConfigurationFile=ConfigurationFile.ini -silent"
+.\Setup.exe /ConfigurationFile=ConfigurationFile.ini
+
+Write-Host 'Done'
+Restart-Computer
---
+++ Release/Scripts_ESXI/November2/4_SSMS.ps1
@@ -0,0 +1,17 @@
+
+# Set file and folder path for SSMS installer .exe
+$folderpath="C:\Users\Administrator\Desktop"
+$filepath="$folderpath\SSMS-Setup-ENU.exe"
+ 
+#If SSMS not present, download
+if (!(Test-Path $filepath)){
+write-host "SSMS-Setup file not found."
+}
+else {
+write-host "Located the SQL SSMS Installer binaries, moving on to install..."
+}
+ 
+# start the SSMS installer
+write-host "Beginning SSMS install..." -nonewline
+Start-Process -Filepath $filepath -ArgumentList -silent -Wait
+Write-Host "SSMS installation complete" -ForegroundColor Green
