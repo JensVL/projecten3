90fbf2b09e68b5b0886967ecb12a2b55b4a2c409
Arno Van Nieuwenhove <arno.vannieuwenhove@student.hogent.be>
2019-11-04 17:19:43 +0100

    Create 1_RUNFIRSTlog.ps1

---
+++ Servers/Bravo2/Testomgeving/Scripts/1_RUNFIRSTlog.ps1
@@ -0,0 +1,22 @@
+# VARIABLES:
+$VBOXdrive = "Z:\"
+# LOG SCRIPT TO FILE (+ op het einde van het script Stop-Transcript doen):
+Start-Transcript "C:\ScriptLogs\1_RUNFIRSTlog.txt"
+Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name ResumeScript `
+                -Value 'C:\Windows\system32\WindowsPowerShell\v1.0\Powershell.exe -executionpolicy bypass -file "$VBOXdrive\2_installDCDNS.ps1"'
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+$DefaultUsername = "Administrator"
+$DefaultPassword = "Admin2019"
+Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
+Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
+Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value $DefaultUsername
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value $DefaultPassword
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 1
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name ForceAutoLogon -Value 1
+invoke-command -filepath c:\Testomgeving\Scripts\1_RUNFIRST.ps1 -computerName Server64
+invoke-command -filepath c:\Testomgeving\Scripts\DNSInstall.ps1 -computerName Server64
+invoke-command -filepath c:\Testomgeving\Scripts\DNSConfig.ps1 -computerName Server64
+Rename-Computer -NewName Bravo2 -Force 
+Stop-Transcript
+Restart-Computer -Force
