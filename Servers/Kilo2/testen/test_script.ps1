50983729948012c2d1c412495adcb0dee4d7f084
Tibo Vanhercke <tibo.vanhercke@student.hogent.be>
2019-11-04 15:26:18 +0100

    Removed Write-Host

--- Servers/Kilo2/testen/test_script.ps1
+++ Servers/Kilo2/testen/test_script.ps1
@@ -3,26 +3,26 @@ $username = "RED\Administrator"
 $credential = New-Object System.Management.Automation.PSCredential($username, $password)
 Start-Job -Name Test -Credential $credential -ScriptBlock {
     $DHCP = Get-WindowsFeature -Name DHCP 
-    Write-Host 'Check name and domain:'
+    'Check name and domain:'
     Get-WmiObject Win32_ComputerSystem | Select-Object -Property Name, Domain | Format-List | Out-String    
-    Write-Host 'Check IP Address:'
+    'Check IP Address:'
     Get-NetIPAddress -AddressFamily IPv4 | Select-Object -Property InterfaceAlias, IPAddress, PrefixLength | Format-List | Out-String
-    Write-Host 'Check Default Gateway:'
+    'Check Default Gateway:'
     Get-NetRoute -AddressFamily IPv4 -DestinationPrefix "0.0.0.0/0" | Select-Object -Property InterfaceAlias, NextHop | Format-List | Out-String
-    Write-Host 'Check DNS Server Addresses:'
+    'Check DNS Server Addresses:'
     Get-DnsClientServerAddress -AddressFamily IPv4 | Select-Object -Property InterfaceAlias, ServerAddresses | Format-List | Out-String
-    Write-Host 'Check if DHCP is installed:'
+    'Check if DHCP is installed:'
     $DHCP | Select-Object -Property Name, InstallState | Format-List | Out-String
-    Write-Host 'Check security groups:'
+    'Check security groups:'
     Get-WmiObject win32_group -Filter "name LIKE 'DHCP%'" | Select-Object -Property Name | Format-List | Out-String
-    Write-Host 'Check if scope is correct:'
+    'Check if scope is correct:'
     $Scope = Get-DhcpServerV4Scope | Select-Object -Property ScopeID, Name, State, SubnetMask, StartRange, EndRange, LeaseDuration, Type
     $Scope | Format-List | Out-String
-    Write-Host 'Check if correct options are configured:'
+    'Check if correct options are configured:'
     $Options = Get-DhcpServerv4OptionValue -ScopeId 172.18.0.0 | Select-Object -Property OptionID, Name, Value
     $Options += (Get-DhcpServerv4OptionValue -OptionId 66,67 | Select-Object -Property OptionID, Name, Value)
     $Options | Sort-Object -Property OptionID | Format-List | Out-String
-    Write-Host 'Check if server is authorized:'
+    'Check if server is authorized:'
     Get-DhcpServerInDC | Select-Object -Property DnsName, IPAddress | Format-List | Out-String
 }
 Wait-Job -Name Test
