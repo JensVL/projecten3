3c01eed7fa8fe082e93b676370f4523fd933af73
Jannes Van Wonterghem <jannes.vanwonterghem@student.hogent.be>
2019-11-12 12:49:54 +0100

    scripts bravo2 en map voor testplannen

---
+++ Release/Scripts_ESXI/Bravo2/DNSConfig.ps1
@@ -0,0 +1,39 @@
+#------------------------------------------------------------------------------
+# Description
+#------------------------------------------------------------------------------
+# Installatiescript dat de configuratie doet van de DNS
+#   (primary forward en reversed lookup zones maken + Forwarder)
+
+#------------------------------------------------------------------------------
+# Variables
+#------------------------------------------------------------------------------
+param(
+    [string]$Bravo2IP    = "172.18.1.67", # DC2 / DNS2
+    [string]$wan_adapter_name = "NAT"
+)
+
+#------------------------------------------------------------------------------
+# Wait for AD services to become available
+#------------------------------------------------------------------------------
+while ($true) {
+    try {
+        Get-ADDomain | Out-Null
+        break
+    } catch {
+        Start-Sleep -Seconds 10
+    }
+}
+
+#------------------------------------------------------------------------------
+# Configure DNS
+#------------------------------------------------------------------------------
+# Fix adapters
+Set-DnsClientServerAddress -InterfaceAlias $wan_adapter_name -ResetServerAddresses
+
+# DNS forwarder instellen op Hogent DNS servers:
+Write-Host ">>> Set DNS forwarder to HoGent DNS"
+Add-DnsServerForwarder -IPAddress 193.190.173.1,193.190.173.2
+
+
+# Disable van overbodige adapter. Nodig voor productie.
+Disable-NetAdapter -Name "NAT" -Confirm:$false
---
+++ Release/Scripts_ESXI/Bravo2/DNSInstall.ps1
@@ -0,0 +1,109 @@
+#------------------------------------------------------------------------------
+# Description
+#------------------------------------------------------------------------------
+# Installatiescript dat zorgt voor de initiële configuratie
+# en de installatie van de ADDS role
+
+#------------------------------------------------------------------------------
+# Variables
+#------------------------------------------------------------------------------
+param(
+    [string]$land                = "eng-BE",
+    [string]$local_ip            = "172.18.1.67",
+    [string]$primary_dc_ip       = "172.18.1.66",
+    [string]$primary_dc_hostname = "Alfa2",
+    [string]$default_gateway     = "172.18.1.65",
+    [int]$lan_prefix             = 27,
+    [string]$domain              = "red.local",
+    [string]$wan_adapter_name    = "NAT",
+    [string]$lan_adapter_name    = "LAN"
+)
+
+
+#------------------------------------------------------------------------------
+# Initial config
+#------------------------------------------------------------------------------
+# Romance standard time = Brusselse tijd
+# Eerste commando zal tijd naar 24uur formaat instellen
+Write-host ">>> Setting correct timezone and time format settings"
+Set-Culture -CultureInfo $land
+set-timezone -Name "Romance Standard Time"
+
+# NAT = de adapter die met het internet verbinding
+# LAN = de adapter met static IP instellingen die alle servers met elkaar verbind.
+Write-host ">>> Changing NIC adapter names"
+# TODO: conditional that checks if adapter names already exists does
+#       not correctly checks for equal strings
+$adaptercount=(Get-NetAdapter | measure).count
+if ($adaptercount -eq 1) {
+    (Get-NetAdapter -Name "Ethernet") | Rename-NetAdapter -NewName $lan_adapter_name
+}
+elseif ($adaptercount -eq 2) {
+    (Get-NetAdapter -Name "Ethernet") | Rename-NetAdapter -NewName $wan_adapter_name
+    (Get-NetAdapter -Name "Ethernet 2") | Rename-NetAdapter -NewName $lan_adapter_name
+}
+
+
+# Prefixlength = CIDR notatie van subnet (in ons geval 255.255.255.224)
+$existing_ip=(Get-NetAdapter -Name $lan_adapter_name | Get-NetIPAddress -AddressFamily IPv4).IPAddress
+if ("$existing_ip" -ne "$local_ip") {
+    Write-host ">>> Setting static ipv4 settings"
+    New-NetIPAddress -InterfaceAlias "$lan_adapter_name" -IPAddress "$local_ip" -PrefixLength $lan_prefix -DefaultGateway "$default_gateway"
+}
+
+# Set DNS of LAN adapter
+Write-Host ">>> Settings DNS of adapter $lan_adapter_name"
+Set-DnsClientServerAddress -InterfaceAlias "$lan_adapter_name" -ServerAddresses($primary_dc_ip,$local_ip)
+
+# Set default password
+Write-Host ">>> Setting default Administrator password"
+$DSRM = ConvertTo-SecureString "Admin2019" -asPlainText -force
+
+# Configure Administrator account
+Write-Host ">>> Configuring Administrator account"
+Set-LocalUser -Name Administrator -AccountNeverExpires -Password $DSRM -PasswordNeverExpires:$true -UserMayChangePassword:$true
+
+# Firewall uitschakelen
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+$existing_ip=(Get-NetAdapter -Name $AdapterNaam | Get-NetIPAddress -AddressFamily IPv4).IPAddress
+if("$existing_ip" -ne "$IpAddress") {
+    Write-host "Setting correct ipv4 settings:" -ForeGroundColor "Green"
+    New-NetIPAddress -InterfaceAlias "$AdapterNaam" -IPAddress "$IpAddress" -PrefixLength $CIDR -DefaultGateway "$default_gateway"
+}
+
+# Joinen van domein "red.local":
+
+$is_AD_domainservices_installed=(Get-WindowsFeature AD-Domain-Services).Installed
+if ("$is_AD_domainservices_installed" -eq 'False') {
+    Write-Host ">>> Installing AD-Domain-Services"
+    Install-WindowsFeature AD-Domain-Services
+}
+
+$is_RSAT_admincenter_installed=(Get-WindowsFeature RSAT-AD-AdminCenter).Installed
+if ("$is_RSAT_admincenter_installed" -eq 'False') {
+    Write-Host ">>> Installing RSAT-AD-AdminCenter"
+    Install-WindowsFeature RSAT-AD-AdminCenter
+}
+
+$is_RSAT_addstools_installed=(Get-WindowsFeature RSAT-ADDS-Tools).Installed
+if ("$is_RSAT_addstools_installed" -eq 'False') {
+    Write-Host ">>> Installing RSAT-ADDS-Tools"
+    Install-WindowsFeature RSAT-ADDS-Tools
+}
+
+$domaincontroller_installed=(Get-ADDomainController 2> $null)
+if (!"$domaincontroller_installed") {
+    Write-Host ">>> Installing AD forest and adding Alfa2 as first DC"
+    Import-Module ADDSDeployment
+
+    $creds = New-Object System.Management.Automation.PSCredential ("RED\Administrator", (ConvertTo-SecureString "Admin2019" -AsPlainText -Force))
+    install-ADDSDomainController -DomainName $domain `
+                  -ReplicationSourceDC "$primary_dc_hostname.$domain" `
+                  -credential $creds `
+                  -installDns:$true `
+                  -createDNSDelegation:$false `
+                  -NoRebootOnCompletion:$true `
+                  -SafeModeAdministratorPassword $DSRM `
+                  -force:$true
+}
