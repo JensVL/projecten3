e49877cf04761c083853c52a104348179fca8824
cevde <32325922+cevde@users.noreply.github.com>
2019-11-12 15:46:09 +0100

    Update 3_SQL.ps1

--- Servers/November2/Scripts/3_SQL.ps1
+++ Servers/November2/Scripts/3_SQL.ps1
@@ -2,13 +2,23 @@
 Add-LocalGroupMember -Group "Administrators" -Member "RED\Administrator"
 Add-LocalGroupMember -Group "Administrators" -Member "RED\Papa2$"
 
-Write-Host 'Installatie SQL Server 2017'
+#Write-Host 'Installatie SQL Server 2017'
 #Installatie SQL Server 2017
-$folderpath="C:\Users\Administrator\Desktop\SQLserver"
-$filepath="$folderpath\Setup.exe"
-Set-location -Path "$folderpath"
-$Parms = " /qs /Install /ConfigurationFile=ConfigurationFile.ini -silent"
-.\Setup.exe /ConfigurationFile=ConfigurationFile.ini
+#$folderpath="C:\Users\Administrator\Desktop\SQLserver"
+#$filepath="$folderpath\Setup.exe"
+#Set-location -Path "$folderpath"
+#$Parms = " /qs /Install /ConfigurationFile=ConfigurationFile.ini -silent"
+#.\Setup.exe /ConfigurationFile=ConfigurationFile.ini
+
+$folderpath="C:\Users\Administrator\Desktop\SQL Server 2017 Installation"
+$filepath="$folderpath\SETUP.exe"
+$paswd="Admin2019" | ConvertTo-SecureString - AsPlainText -Force
+.$folderpath\SETUP.exe /Q /ACTION=Install /IACCEPTSQLSERVERLICENSETERMS /Features=SQL /INSTANCENAME=MSSQLSERVER /INSTANCEID=MSSQLSERVER /SQLSVACCOUNT="RED\Administrator" `
+/SQLSVCPASSWORD="$paswd" /AGTSVCACCOUNT="RED\Administrator" /AGTSVCPASSWORD="$paswd" /FTSVACCOUNT="RED\Administrator" /FTSVCPASSWORD="$paswd" `
+/SQLSYSADMINACCOUNTS="RED\Administrator" /INSTALLSQLDATADIR=C:\SQLServer /SQLUSERDBDIR="C:\Database" /SQLUSERDBLOGDIR="C:\DBlogs" `
+/SQLBACKUPDIR="C:\Backup" /SQLTEMPDBDIR="C:\TempDB" /SQLTEMPDBLOGDIR="C:\TempDBlog" `
+/TCPENABLED="1" `
+/NPENABLED="1"
 
 Write-Host 'Done'
 
