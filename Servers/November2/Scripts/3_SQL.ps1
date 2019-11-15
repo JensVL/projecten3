7e4f1606803bade77ac834409751a0bfb5163180
cevde <32325922+cevde@users.noreply.github.com>
2019-11-15 16:07:05 +0100

    Update 3_SQL.ps1

--- Servers/November2/Scripts/3_SQL.ps1
+++ Servers/November2/Scripts/3_SQL.ps1
@@ -11,28 +11,29 @@ Write-Host 'Done'
 Restart-Computer
 
 
-*/
-$folderpath="C:\Users\Administrator.RED\Documents"
-$filepath="$folderpath\SETUP.exe"
-Write-Host 'Deel 1...'
-$paswd="Admin2019" | ConvertTo-SecureString -AsPlainText -Force
-Write-Host 'Deel 2...'
-.$folderpath\SETUP.exe /Q /ACTION=Install /IACCEPTSQLSERVERLICENSETERMS /Features=SQL /INSTANCENAME=MSSQLSERVER /INSTANCEID=MSSQLSERVER `
-/SQLSVCACCOUNT="RED\Administrator" `
-/SQLSVCPASSWORD="$paswd" `
-/AGTSVCACCOUNT="RED\Administrator" `
-/AGTSVCPASSWORD="$paswd" `
-/FTSVCACCOUNT="RED\Administrator" `
-/FTSVCPASSWORD="$paswd" `
-/SQLSysAdminAccounts = @('RED\Administrators') `
-/INSTALLSQLDATADIR="$folderpath\SQLServer" `
-/SQLUSERDBDIR="$folderpath\Database" `
-/SQLUSERDBLOGDIR="$folderpath\DBlogs" `
-/SQLBACKUPDIR="$folderpath\Backup" `
-/SQLTEMPDBDIR="$folderpath\TempDB" `
-/SQLTEMPDBLOGDIR="$folderpath\TempDBlog" `
-/TCPENABLED="1" `
-/NPENABLED="1"
 
-Write-Host 'Done'
-/*
+
+# $folderpath="C:\Users\Administrator.RED\Documents"
+# $filepath="$folderpath\SETUP.exe"
+# Write-Host 'Deel 1...'
+# $paswd="Admin2019" | ConvertTo-SecureString -AsPlainText -Force
+# Write-Host 'Deel 2...'
+# .$folderpath\SETUP.exe /Q /ACTION=Install /IACCEPTSQLSERVERLICENSETERMS /Features=SQL /INSTANCENAME=MSSQLSERVER /INSTANCEID=MSSQLSERVER `
+# /SQLSVCACCOUNT="RED\Administrator" `
+# /SQLSVCPASSWORD="$paswd" `
+# /AGTSVCACCOUNT="RED\Administrator" `
+# /AGTSVCPASSWORD="$paswd" `
+# /FTSVCACCOUNT="RED\Administrator" `
+# /FTSVCPASSWORD="$paswd" `
+# /SQLSysAdminAccounts = @('RED\Administrators') `
+# /INSTALLSQLDATADIR="$folderpath\SQLServer" `
+# /SQLUSERDBDIR="$folderpath\Database" `
+# /SQLUSERDBLOGDIR="$folderpath\DBlogs" `
+# /SQLBACKUPDIR="$folderpath\Backup" `
+# /SQLTEMPDBDIR="$folderpath\TempDB" `
+# /SQLTEMPDBLOGDIR="$folderpath\TempDBlog" `
+# /TCPENABLED="1" `
+# /NPENABLED="1"
+
+# Write-Host 'Done'
+
