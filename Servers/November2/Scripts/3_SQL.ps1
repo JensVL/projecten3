35e9344d15927ee879db299544db52c0f24fd3e0
cevde <32325922+cevde@users.noreply.github.com>
2019-11-15 11:40:25 +0100

    Update 3_SQL.ps1

--- Servers/November2/Scripts/3_SQL.ps1
+++ Servers/November2/Scripts/3_SQL.ps1
@@ -10,10 +10,10 @@ Add-LocalGroupMember -Group "Administrators" -Member "RED\Papa2$"
 #$Parms = " /qs /Install /ConfigurationFile=ConfigurationFile.ini -silent"
 #.\Setup.exe /ConfigurationFile=ConfigurationFile.ini
 
-$folderpath="C:\Users\Administrator\Desktop\SQL Server 2017 Installation"
+$folderpath="C:\Users\Administrator.RED\Documents"
 $filepath="$folderpath\SETUP.exe"
 $paswd="Admin2019" | ConvertTo-SecureString - AsPlainText -Force
-.$folderpath\SETUP.exe /Q /ACTION=Install /IACCEPTSQLSERVERLICENSETERMS /Features=SQL /INSTANCENAME=MSSQLSERVER /INSTANCEID=MSSQLSERVER /SQLSVACCOUNT="RED\Administrator" `
+.$folderpath\SETUP.exe /Q /ACTION=Install /IACCEPTSQLSERVERLICENSETERMS /Features=SQL /INSTANCENAME="MSSQLSERVER" /INSTANCEID="MSSQLSERVER" /SQLSVCACCOUNT="RED\Administrator" `
 /SQLSVCPASSWORD="$paswd" /AGTSVCACCOUNT="RED\Administrator" /AGTSVCPASSWORD="$paswd" /FTSVACCOUNT="RED\Administrator" /FTSVCPASSWORD="$paswd" `
 /SQLSYSADMINACCOUNTS="RED\Administrator" /INSTALLSQLDATADIR=C:\SQLServer /SQLUSERDBDIR="C:\Database" /SQLUSERDBLOGDIR="C:\DBlogs" `
 /SQLBACKUPDIR="C:\Backup" /SQLTEMPDBDIR="C:\TempDB" /SQLTEMPDBLOGDIR="C:\TempDBlog" `
