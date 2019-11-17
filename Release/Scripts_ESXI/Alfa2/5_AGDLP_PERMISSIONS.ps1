a83462e29565ac807baa5afc4d8d9136b0633755
LAURENS BLANCQUAERTCASSAER <laurens.blancquaertcassaer@student.hogent.be>
2019-11-17 16:17:40 +0100

    Alfa2 scripts toegevoegd een release branch

---
+++ Release/Scripts_ESXI/Alfa2/5_AGDLP_PERMISSIONS.ps1
@@ -0,0 +1,210 @@
+# Installatiescript dat de configuratie doet van de DNS (primary forward en reversed lookup zones maken + Forwarder):
+                          # TODO: Eigenlijk kan dit script pas gestart worden vanaf dat lima2 volledig klaar is KLOPT DIT??????????????????????? TODO #
+# Elke stap wordt uitgelegd met zijn eigen comment
+
+# VARIABLES:
+$VBOXdrive = "Z:"
+
+$VerkoopData = "\\Lima2\VerkoopData\"
+$OntwikkelingData = "\\Lima2\OntwikkelingData\"
+$ITData = "\\Lima2\ITData\"
+$DirData = "\\Lima2\DirData\"
+$AdminData = "\\Lima2\AdminData\"
+$ShareVerkoop = "\\Lima2\ShareVerkoop\"
+
+$Homedirs = "\\Lima2\Homedirs\"
+$Profiledirs = "\\Lima2\Profiledirs\"
+
+# PREFERENCE VARIABLES: (Om Debug,Verbose en informaation info in de Start-Transcript log files te zien)
+$DebugPreference = "Continue"
+$VerbosePreference = "Continue"
+$InformationPreference = "Continue"
+
+
+# LOG SCRIPT TO FILE (+ op het einde van het script Stop-Transcript doen):
+Start-Transcript "C:\ScriptLogs\5_AGDLP_PERMISSIONSlog.txt"
+
+# 1) Map alle drives zodat Alfa2 toegang heeft tot de shared folders van Lima2:          TODO: MOET OOK IN SCCM TASK SEQUENCE VOOR DE CLIENTS (+ SMB 1.0/CIFS)
+# net use [drive letter]: \\server\sharedFolder\    /P:yes = Maak mapping permanent
+Write-Host "Mapping Shared folders of Lima2 to drive letters so Alfa2 has access to them:" -ForeGroundColor "Green"
+net use e: $VerkoopData /P:Yes
+net use f: $OntwikkelingData /P:Yes
+net use g: $ITData /P:Yes
+net use h: $DirData /P:Yes
+net use i: $AdminData /P:Yes
+net use j: $ShareVerkoop /P:Yes
+
+net use x: $Homedirs /P:Yes           # Y: drive in opgave
+net use y: $Profiledirs /P:Yes        # Z: drive in opgave (was al bezet door VirtualBox shared folder)
+
+
+# 2) Domain local groups aanmaken waarop we de permissies toepassen:
+# IT_Administratie heeft full control over alle shared folders
+Write-Host "Make AD Localdomain groups (= permission groups)" -ForeGroundColor "Green"
+
+New-ADGroup -Name "RW_VERKOOP" -Path "DC=red,DC=local" -GroupCategory "Security" -GroupScope "DomainLocal"
+New-ADGroup -Name "RW_ONTWIKKELING" -Path "DC=red,DC=local" -GroupCategory "Security" -GroupScope "DomainLocal"
+New-ADGroup -Name "RW_DIRECTIE" -Path "DC=red,DC=local" -GroupCategory "Security" -GroupScope "DomainLocal"
+New-ADGroup -Name "RW_ADMIN" -Path "DC=red,DC=local" -GroupCategory "Security" -GroupScope "DomainLocal"
+
+# IT DomainLocal group dat full permissies aan alle shares:
+New-ADGroup -Name "FullAccess_IT" -Path "DC=red,DC=local" -GroupCategory "Security" -GroupScope "DomainLocal"
+
+# Homedirs/profiledirs enkel zichtbaar voor de gebruiker die is aangemeld (de anderen niet):
+New-ADGroup -Name "RW_HOMEDIRS" -Path "DC=red,DC=local" -GroupCategory "Security" -GroupScope "DomainLocal"
+New-ADGroup -Name "RW_PROFILEDIRS" -Path "DC=red,DC=local" -GroupCategory "Security" -GroupScope "DomainLocal"
+
+New-ADGroup -Name "ALLE_AFDELINGEN" -Path "DC=red,DC=local" -GroupCategory "Security" -GroupScope "DomainLocal"
+
+New-ADGroup -Name "R_SHAREVERKOOP" -Path "DC=red,DC=local" -GroupCategory "Security" -GroupScope "DomainLocal"
+New-ADGroup -Name "RW_SHAREVERKOOP" -Path "DC=red,DC=local" -GroupCategory "Security" -GroupScope "DomainLocal"
+
+# 3) Voeg de Domain local groups toe als members juiste van de global groups:
+Add-LocalGroupMember -Group "RW_VERKOOP" -Member "Verkoop"
+Add-LocalGroupMember -Group "RW_ONTWIKKELING" -Member "Ontwikkeling"
+Add-LocalGroupMember -Group "RW_DIRECTIE" -Member "Directie"
+Add-LocalGroupMember -Group "RW_ADMIN" -Member "Administratie"
+
+Add-LocalGroupMember -Group "FullAccess_IT" -Member "IT_Administratie"
+
+# PROFILESDIR EN HOMEDIRS => alle afdelingen in ALLE_AFDELINGEN group:
+Add-LocalGroupMember -Group "ALLE_AFDELINGEN" -Member "Verkoop","Ontwikkeling","Directie","Administratie","IT_Administratie"
+
+# Shareverkoop RW en R permissies apart:
+Add-LocalGroupMember -Group "R_SHAREVERKOOP" -Member "Ontwikkeling"
+Add-LocalGroupMember -Group "RW_SHAREVERKOOP" -Member "Verkoop"
+
+# 4) Geef de nodige permissies/rechten op de shared folders volgens de opdrachtomschrijving:
+Write-Host "Assigning permissions to the Lima2:" -ForeGroundColor "Green"
+
+
+# 4.1) Verkoopdata: (+ RW voor ShareVerkoop folder)
+$ACL = Get-ACL "$Verkoopdata"
+$Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("RW_VERKOOP","Read", `
+                         "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Read permissions
+$ACL.SetAccessRule($Permissies)
+
+$Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("RW_VERKOOP","Write", `
+                         "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Write permissions
+$ACL.AddAccessRule($Permissies)
+
+Set-ACL "$Verkoopdata" $ACL
+# ook read write permissions aan shareverkoop folder geven:
+Set-ACL "$ShareVerkoop" $ACL
+
+
+# 4.2) Ontwikkelingdata:
+$ACL = Get-ACL "$OntwikkelingData"
+$Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("RW_ONTWIKKELING","Read", `
+                         "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Read permissions
+$ACL.SetAccessRule($Permissies)
+
+$Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("RW_ONTWIKKELING","Write", `
+                         "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Write permissions
+$ACL.AddAccessRule($Permissies)
+
+Set-ACL "$OntwikkelingData" $ACL
+
+
+# 4.3) Dirdata:
+$ACL = Get-ACL "$DirData"
+$Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("RW_DIRECTIE","Read", `
+                         "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Read permissions
+$ACL.SetAccessRule($Permissies)
+
+$Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("RW_DIRECTIE","Write", `
+                         "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Write permissions
+$ACL.AddAccessRule($Permissies)
+
+Set-ACL "$DirData" $ACL
+
+
+# 4.4) Admindata:
+$ACL = Get-ACL "$AdminData"
+$Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("RW_ADMIN","Read", `
+                         "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Read permissions
+$ACL.SetAccessRule($Permissies)
+
+$Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("RW_ADMIN","Write", `
+                         "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Write permissions
+$ACL.AddAccessRule($Permissies)
+
+Set-ACL "$AdminData" $ACL
+
+
+# 4.5) Full Access IT data:
+$ACL = Get-ACL "$ITData"
+$Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("FullAccess_IT","FullControl", `
+                         "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Full Control for FullAccess_IT group
+$ACL.SetAccessRule($Permissies)
+Set-ACL "$ITData" $ACL
+
+Set-ACL "$VerkoopData" $ACL
+Set-ACL "$OntwikkelingData" $ACL
+Set-ACL "$DirData" $ACL
+Set-ACL "$AdminData" $ACL
+Set-ACL "$Shareverkoop" $ACL
+Set-ACL "$Profiledirs" $ACL
+Set-ACL "$Homedirs" $ACL
+
+
+# 4.6 Shareverkoop:
+$ACL = Get-ACL "$ShareVerkoop"
+$Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("RW_ONTWIKKELING","Read", `
+                         "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Read permissions ontwikkeling
+$ACL.SetAccessRule($Permissies)
+
+Set-ACL "$ShareVerkoop" $ACL
+
+
+# 4.7) Profile en home dirs algemeen per afdeling read / write:
+      $ACL = Get-Acl "$Homedirs"
+      $Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("ALLE_AFDELINGEN","Read", `
+                               "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Read permissions ontwikkeling
+      $ACL.SetAccessRule($Permissies)
+      $Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("ALLE_AFDELINGEN","Write", `
+                               "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Write permissions verkoop
+      $ACL.AddAccessRule($Permissies)
+
+      Set-ACL "$Homedirs" $ACL
+      Set-ACL "$Profiledirs" $ACL
+
+# 5) Profile en Home dirs individueel per gebruiker:
+ $REDLOCALUSERS = @(
+                  "KimberlyDC", "ArnoVN", "LaurensBC", "FerreV", "LeviG", "AronM", "JensVL", "JoachimVDK", "TiboV", "YngvarS",
+                  "TimG", "RikC", "JannesVW", "JonasV", "CedricVDE", "CedricD", "RobinVDW", "MatthiasVDV", "RobbyD", "NathanC",
+                  "EliasW", "AlisterA", "SeanV"
+                  )
+
+Write-Host "Assigning correct permissions to each AD user's profile and home directory:" -ForeGroundColor "Green"
+ForEach ($GEBRUIKER in $REDLOCALUSERS) {
+#     HOMEFOLDERS:  Permissions op modify zetten
+      $HomeFolder = "$Homedirs" + "$GEBRUIKER"
+
+      $ACL = Get-ACL "$HomeFolder"
+
+      $Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("$GEBRUIKER","Modify", `
+                               "ContainerInherit,ObjectInherit","InheritOnly","Allow")  #  Modify permissions
+      $ACL.SetAccessRule($Permissies)
+
+      Set-ACL "$HomeFolder" $ACL
+
+#    PROFILE FOLDERS: Permissions op modify zetten
+      $ProfileFolder = "$Profiledirs" + "$GEBRUIKER"
+      $ACL = Get-ACL "$ProfileFolder"
+
+      $Permissies = New-Object System.Security.AccessControl.FileSystemAccessRule("$GEBRUIKER","Modify", `
+                               "ContainerInherit,ObjectInherit","InheritOnly","Allow")  # Modify permissions
+      $ACL.SetAccessRule($Permissies)
+
+      Set-ACL "$ProfileFolder" $ACL
+
+      # Maak van elke gebruiker de profile en homefolder directories aan:
+      Set-AdUser -SAMaccountname "$GEBRUIKER" -HomeFolder "$HomeFolder" -HomeDrive "X:" -ProfilePath "$ProfileFolder"
+ }
+
+# 6) Zet Auto login terug af:
+ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 0
+ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name ForceAutoLogon -Value 0
+
+Stop-Transcript
---
+++ Release/Scripts_ESXI/Alfa2/999_PrepareADforSCCM.ps1
@@ -0,0 +1,89 @@
+﻿# Dit script moet uitgevoerd worden na de installatie van beide DC'S en nadat Papa2 (SCCM Deployment Server) !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! NIET VERGETEN UIT TE VOEREN NADAT PAPA2 IN DOMEIN ZIT EN VOOR SCCM INSTALLATIe!
+# aan het domein is toegevoegd Het script zal de AD voorbereiden op de installatie van SCCM
+# en ervoor zorgen dat alles goed werkt
+# Elke stap wordt uitgelegd met zijn eigen comment
+
+############################################## BELANGRIJK #####################################################################################
+# VOER DIT SCRIPT UIT ALS ADMINISTRATOR ACCOUNT RED\Administrator (password= Admin2019) TIJDENS DE DEMO
+############################################## BELANGRIJK #####################################################################################
+
+# VARIABLES:
+# --------------------------------------------------------------------------------------------------------
+# VOOR INTEGRATIE:
+$VBOXdrive = "C:\Scripts_ESXI\Alfa2"
+
+# VOOR VIRTUALBOX TESTING:
+#$VBOXdrive = "Z:"
+# --------------------------------------------------------------------------------------------------------
+
+$Account = "RED\Administrator"
+
+# PREFERENCE VARIABLES: (Om Debug,Verbose en informaation info in de Start-Transcript log files te zien)
+$DebugPreference = "Continue"
+$VerbosePreference = "Continue"
+$InformationPreference = "Continue"
+
+# LOG SCRIPT TO FILE (+ op het einde van het script Stop-Transcript doen):
+Start-Transcript "C:\ScriptLogs\999_PrepareADforSCCMlog.txt"
+
+# 1) Password instellen op Admin2019
+$password=ConvertTo-SecureString "Admin2019" -asPlainText -force
+
+# 2) Het aanmaken van de Papa2 computer zelf:
+New-ADComputer -Name "Papa2"
+
+# 3) Nu moeten we in ADSIedit (Waar al de instellingen van ADDS staan) een System Management container maken onder de "System"
+# 3.1) Verbind met ADSIedit:
+# Opmerking: cn=System Zal rechtsreeks verbinding maken met de System container waarin wij onze container in willen opslaan:
+$ASDIconnection = [ADSI]"LDAP://localhost:389/cn=System,dc=red,dc=local"
+
+# 3.2) Maak de System Management container aan in ADSIedit:
+Write-Host "Creating System Management container in ADSIedit:" -ForeGroundColor "Green"
+$SysManContainer = $ASDIconnection.Create("container", "cn=System Management")
+$SysManContainer.SetInfo()
+
+# 4) Nu moeten we de permissies (ALLE PERMISSIES!) van deze System Management container delegeren aan de Papa2 server
+# Belangrijk hier is dat je ook de instelling "Applies to this object and all descendant objects" moet selecteren
+# 4.1) Geef ALLE permissies van deze container aan de Papa2 server:
+# Connect met System Management container:
+$SystemManagementCN = [ADSI]("LDAP://localhost:389/cn=System Management,cn=System,dc=red,dc=local")
+
+# 4.2) Get Papa2 als computer object:
+$SCCMserver = get-adcomputer "Papa2"
+
+# 4.3) Get identity reference van het computer object:
+$SID = [System.Security.Principal.SecurityIdentifier] $SCCMserver.SID
+$ServerIdentity = [System.Security.Principal.IdentityReference] $SID
+
+# 4.4) Stel permissions in als ALLOW full control (full control = GenericAll hieronder):
+# ActiveDirectorySecurityInheritance = Stel permissies in op:
+# de System Management container zelf + ALLE DESCENDANT OBJECTS!
+$permissions = [System.DirectoryServices.ActiveDirectoryRights] "GenericAll"
+$allow = [System.Security.AccessControl.AccessControlType] "Allow"
+$inheritanceAll = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "All"
+
+# 4.5) Dit zal de gekozen permissies toepassen op de System Management container:
+# in ADSIedit noemt een permission rule "Access rule" (AddAccessRule methode)
+Write-Host "Setting permissions of System Management container to Papa2 server" -ForeGroundColor "Green"
+$PermissionsRule = New-Object System.DirectoryServices.ActiveDirectoryAccessRule `
+$ServerIdentity,$permissions,$allow,$inheritanceAll
+
+$SystemManagementCN.psbase.ObjectSecurity.AddAccessRule($PermissionsRule)
+$SystemManagementCN.psbase.commitchanges()
+
+# 5) Extend Active Directory Schema:
+# In de SCCM installation folder zit een script (extadsch) dat je moet uitvoeren dat je AD zal uitbereiden
+# met additionele attributen waar SCCM gebruik van maakt.
+# Dit script staat klaar in de virtualbox shared folder root/ExtendADschema
+
+# 5.1) Eerst de folder "ExtendADSchema" uit de VB share folder lokaal kopiëren naar Alfa2 server:
+# Dit moet omdat je anders een warning "This file might be unsafe" krijgt.
+Copy-Item "$VBOXdrive\ExtendADschema" -Destination "C:\Users\Administrator\Desktop" -Recurse
+
+# 5.2) Dan voer je het script (ALS ADMINISTRATOR!) uit om de AD schema te extenden:
+Write-Host "Extending AD Schema:" -ForeGroundColor "Green"
+Start-Process "C:\Users\Administrator\Desktop\ExtendADschema\extadsch.exe"
+
+# C:\ExtADSch.log check deze log file om te zien of extenden van het AD schema succesvol was
+
+Stop-Transcript
