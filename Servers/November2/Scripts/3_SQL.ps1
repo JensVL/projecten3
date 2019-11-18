a587b0c67257e790efc8128c8f81d31c27f9c203
AronMarckx <aronmarckx@hotmail.com>
2019-11-18 09:33:01 +0100

    Update 3_SQL.ps1

--- Servers/November2/Scripts/3_SQL.ps1
+++ Servers/November2/Scripts/3_SQL.ps1
@@ -4,36 +4,36 @@ Add-LocalGroupMember -Group "Administrators" -Member "RED\Papa2$"
 
 Write-Host 'Installatie SQL Server 2017'
 #Installatie SQL Server 2017
-$folderpath="C:\Users\Administrator.RED\Documents"
+#$folderpath="C:\Users\Administrator\Desktop\SQL Server 2017 Installation"
+#$filepath="$folderpath\Setup.exe"
+#Set-location -Path "$folderpath"
+#$Parms = " /qs /Install /ConfigurationFile=ConfigurationFile.ini -silent"
+#.\Setup.exe /ConfigurationFile=ConfigurationFile.ini
+
+
+$folderpath="C:\Users\Administrator\Desktop\SQL Server 2017 Installation"
 $filepath="$folderpath\Setup.exe"
-Set-location -Path "$folderpath"
-$Parms = " /qs /Install /ConfigurationFile=ConfigurationFile.ini -silent"
-.\Setup.exe /ConfigurationFile=ConfigurationFile.ini
 
-
-
-
-# $folderpath="C:\Users\Administrator.RED\Documents"
-# $filepath="$folderpath\SETUP.exe"
-# Write-Host 'Deel 1...'
-# $paswd="Admin2019" | ConvertTo-SecureString -AsPlainText -Force
-# Write-Host 'Deel 2...'
-# .$folderpath\SETUP.exe /Q /ACTION=Install /IACCEPTSQLSERVERLICENSETERMS /Features=SQL /INSTANCENAME=MSSQLSERVER /INSTANCEID=MSSQLSERVER `
-# /SQLSVCACCOUNT="RED\Administrator" `
-# /SQLSVCPASSWORD="$paswd" `
-# /AGTSVCACCOUNT="RED\Administrator" `
-# /AGTSVCPASSWORD="$paswd" `
-# /FTSVCACCOUNT="RED\Administrator" `
-# /FTSVCPASSWORD="$paswd" `
-# /SQLSysAdminAccounts = @('RED\Administrators') `
-# /INSTALLSQLDATADIR="$folderpath\SQLServer" `
-# /SQLUSERDBDIR="$folderpath\Database" `
-# /SQLUSERDBLOGDIR="$folderpath\DBlogs" `
-# /SQLBACKUPDIR="$folderpath\Backup" `
-# /SQLTEMPDBDIR="$folderpath\TempDB" `
-# /SQLTEMPDBLOGDIR="$folderpath\TempDBlog" `
-# /TCPENABLED="1" `
-# /NPENABLED="1"
+Write-Host 'Deel 1...'
+$paswd="Admin2019" | ConvertTo-SecureString -AsPlainText -Force
+$SCCMPassword = [System.Runtime.Interopservices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($paswd))
+Write-Host 'Deel 2...'
+.$folderpath\SETUP.exe /Q /ACTION=Install /IACCEPTSQLSERVERLICENSETERMS /Features=SQL /INSTANCENAME=MSSQLSERVER /INSTANCEID=MSSQLSERVER `
+/SQLSVCACCOUNT="RED\Administrator" `
+/SQLSVCPASSWORD="$SCCMPassword" `
+/AGTSVCACCOUNT="RED\Administrator" `
+/AGTSVCPASSWORD="$SCCMPassword" `
+/FTSVCACCOUNT="RED\Administrator" `
+/FTSVCPASSWORD="$SCCMPassword" `
+/SQLSysAdminAccounts= 'RED\Administrator' `
+/INSTALLSQLDATADIR="C:\SQLServer" `
+/SQLUSERDBDIR="C:\Database" `
+/SQLUSERDBLOGDIR="C:\DBlogs" `
+/SQLBACKUPDIR="C:\Backup" `
+/SQLTEMPDBDIR="C:\TempDB" `
+/SQLTEMPDBLOGDIR="C:\TempDBlog" `
+/TCPENABLED="1" `
+/NPENABLED="1"
 
 # Write-Host 'Done'
 
