b402d1d65d323d5e2217b8bb57784942de263a40
ArnoVanNieuwenhove <36506240+ArnoVanNieuwenhove@users.noreply.github.com>
2019-11-18 11:51:53 +0100

    Update 1_RUNFIRST.ps1

--- Servers/Bravo2/Testomgeving/Scripts/1_RUNFIRST.ps1
+++ Servers/Bravo2/Testomgeving/Scripts/1_RUNFIRST.ps1
@@ -17,7 +17,6 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlo
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 1
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name ForceAutoLogon -Value 1
 
-invoke-command -filepath $VBOXdrive\1_RUNFIRST.ps1
 invoke-command -filepath $VBOXdrive\DNSInstall.ps1
 invoke-command -filepath $VBOXdrive\DNSConfig.ps1
 
